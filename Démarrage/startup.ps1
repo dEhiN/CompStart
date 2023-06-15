4205b73f661276a6156da7993c1f67d39b216143
David H. Watson <hiranwatson@gmail.com>
2023-06-15 16:34:45 -0400

    Variable renaming

--- Démarrage/startup.ps1
+++ Démarrage/startup.ps1
@@ -12,22 +12,15 @@ do {
         $LoopTrue = $False
 
         # Set up standard working tabs for internal resources: Gmail, GSheet "Client Support Processes", GDoc "Les gabarits pour utiliser avec Halo"
-        $DealerFXInternalTabs = @(
+        $DealerFXChromeOneTabs = @(
             "https://mail.google.com/mail/u/0/#inbox",
             "https://docs.google.com/spreadsheets/d/1ZmCBNPA40Ixpcrkl7BmSZdFdrvrxmotk1eIRybobDAM/",
             "https://docs.google.com/document/d/1vrlXzHkL_SDu3-y2MksTYEL-ZAamWQ8hE-ck7Q3Figs/edit"
         )
-        $DealerFXInternalURLs = [string]$DealerFXInternalTabs
-
-        # Set up working tabs pertaining to tickets: GSheet "New Email - ALL OEM", GSheet "BLoc-notes des billets"
-        $DealerFXTicketTabs = @(
-            "https://docs.google.com/spreadsheets/d/1165JnOAgr0JkNRu7lIWUD_OmbwsSLWfDvx3Ha8JvSKk/",
-            "https://docs.google.com/spreadsheets/d/1q5NXZtm0Ln94gMi8FapfNvtbviVzrtcnS8IBUiH5Qic/"
-        )
-        $DealerFXTicketURLs = [string]$DealerFXTicketTabs
+        $DealerFXChromeOneURLs = [string]$DealerFXChromeOneTabs
 
         # Set up standard working tabs for external resources: Prod1 One Platform, Kibana Prod1, Prod2 One Platform, Kibana Prod2, AWS Connect - Real Time Agent and Queue stats, AWS Agent App
-        $DealerFXExternalTabs = @(
+        $DealerFXChromeTwoTabs = @(
             "https://chrysler1.advisordashboard.net/logins/Login.2.aspx",
             "https://vpc-prod1-es01-logs-tt2fnqcrw3ks5r6uangob7sy6u.us-east-1.es.amazonaws.com/_plugin/kibana/app/kibana#/discover?_g=()&_a=(columns:!(_source),index:fa06ab90-d9d6-11e8-a98f-e9ebcc5de641,interval:auto,query:(language:lucene,query:''),sort:!('@timestamp',desc))",
             "https://service.dealer-fx.com/logins/Login.2.aspx",
@@ -35,19 +28,30 @@ do {
             "https://dfx1.my.connect.aws/real-time-metrics?reportArn=arn:aws:connect:us-east-1:153590104080:instance/2add78db-445c-4fbe-890e-a2cf0595de6e/report/bd7a1764-370f-4ff9-8cef-ee9c74a54be5",
             "https://master.d3pfi7mlwkgahz.amplifyapp.com/"
         )
-        $DealerFXExternalURLs = [string]$DealerFXExternalTabs
+        $DealerFXChromeTwoURLs = [string]$DealerFXChromeTwoTabs
+
+        # Set up working tabs pertaining to tickets: GSheet "New Email - ALL OEM", GSheet "BLoc-notes des billets"
+        $DealerFXChromeThreeTabs = @(
+            "https://docs.google.com/spreadsheets/d/1165JnOAgr0JkNRu7lIWUD_OmbwsSLWfDvx3Ha8JvSKk/",
+            "https://docs.google.com/spreadsheets/d/1q5NXZtm0Ln94gMi8FapfNvtbviVzrtcnS8IBUiH5Qic/"
+        )
+        $DealerFXChromeThreeURLs = [string]$DealerFXChromeThreeTabs
         
         # Open all the programs for startup
         # Chrome - Dealer-FX standard working tabs for internal resources
-        Start-Process -FilePath "C:\Program Files\Google\Chrome\Application\chrome.exe" -ArgumentList "--profile-directory=Default","--new-window",$DealerFXInternalURLs
-        # Chrome - Dealer-FX working tabs for tickets
-        Start-Process -FilePath "C:\Program Files\Google\Chrome\Application\chrome.exe" -ArgumentList "--profile-directory=Default","--new-window",$DealerFXTicketURLs
+        Start-Process -FilePath "C:\Program Files\Google\Chrome\Application\chrome.exe" -ArgumentList "--profile-directory=Default","--new-window",$DealerFXChromeOneURLs
         # Chrome - Dealer-FX standard working tabs for external resources
-        Start-Process -FilePath "C:\Program Files\Google\Chrome\Application\chrome.exe" -ArgumentList "--profile-directory=Default","--new-window",$DealerFXExternalURLs
+        Start-Process -FilePath "C:\Program Files\Google\Chrome\Application\chrome.exe" -ArgumentList "--profile-directory=Default","--new-window",$DealerFXChromeTwoURLs
+        # Chrome - Dealer-FX working tabs for tickets
+        Start-Process -FilePath "C:\Program Files\Google\Chrome\Application\chrome.exe" -ArgumentList "--profile-directory=Default","--new-window",$DealerFXChromeThreeURLs
         # Google Chat Chrome app
         Start-Process -FilePath "C:\Program Files\Google\Chrome\Application\chrome_proxy.exe" -ArgumentList "--profile-directory=Default", "--app-id=mdpkiolbdkhdjpekfbkbmhigcaggjagi"
         # Halo Chrome app
         Start-Process -FilePath "C:\Program Files\Google\Chrome\Application\chrome_proxy.exe" -ArgumentList "--profile-directory=Default","--app-id=ifgfkkbichmgomaifmnecbnpibfepmco"
+        # Zoom
+        Start-Process -FilePath "C:\Program Files\Zoom\bin\Zoom.exe"
+        # FortiClient VPN
+        Start-Process -FilePath "C:\Program Files\Fortinet\FortiClient\FortiClient.exe"
 
     } elseif (($UserPrompt -eq "N") -or ($UserPrompt -eq "n")) {
 
