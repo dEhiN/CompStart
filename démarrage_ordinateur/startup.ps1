a9e60ecf22d3488fba0e6d843e17dc56c7367354
David H. Watson <hiranwatson@gmail.com>
2023-06-27 11:34:12 -0400

    Updated startup.ps1 to be same as main branch
    I switched branches to feature.update.one so needed to replicate changes

--- démarrage_ordinateur/startup.ps1
+++ démarrage_ordinateur/startup.ps1
@@ -11,32 +11,33 @@ do {
         # Tell loop to quit
         $LoopTrue = $False
 
-        # Set up standard working tabs for internal resources: Gmail, GSheet "Client Support Processes", GDoc "Les gabarits pour utiliser avec Halo"
+        # Set up standard working tabs for internal resources: Gmail, GSheet "Client Support Processes", Google Calendar
         $DealerFXChromeOneTabs = @(
             "https://mail.google.com/mail/u/0/#inbox",
             "https://docs.google.com/spreadsheets/d/1ZmCBNPA40Ixpcrkl7BmSZdFdrvrxmotk1eIRybobDAM/",
-            "https://docs.google.com/document/d/1vrlXzHkL_SDu3-y2MksTYEL-ZAamWQ8hE-ck7Q3Figs/edit"
+            "https://docs.google.com/document/d/1vrlXzHkL_SDu3-y2MksTYEL-ZAamWQ8hE-ck7Q3Figs/edit",
+            "https://calendar.google.com/calendar/u/0/r"
         )
         $DealerFXChromeOneURLs = [string]$DealerFXChromeOneTabs
 
-        # Set up standard working tabs for external resources: Prod1 One Platform, Kibana Prod1, Prod2 One Platform, Kibana Prod2, AWS Connect - Metrics Report, AWS Agent App
+        # Set up standard working tabs for external resources: Prod1 One Platform, Kibana Prod1, Prod2 One Platform, Kibana Prod2, AWS Connect - Real Time Agent and Queue stats
         $DealerFXChromeTwoTabs = @(
             "https://chrysler1.advisordashboard.net/logins/Login.2.aspx",
             "https://vpc-prod1-es01-logs-tt2fnqcrw3ks5r6uangob7sy6u.us-east-1.es.amazonaws.com/_plugin/kibana/app/kibana#/discover?_g=()&_a=(columns:!(_source),index:fa06ab90-d9d6-11e8-a98f-e9ebcc5de641,interval:auto,query:(language:lucene,query:''),sort:!('@timestamp',desc))",
             "https://service.dealer-fx.com/logins/Login.2.aspx",
             "https://vpc-prod2-es-logs-uf3z64zx5gyuk3chonnjhglbpy.us-east-1.es.amazonaws.com/_plugin/kibana/app/discover#/?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-15m,to:now))&_a=(columns:!(_source),filters:!(),index:b0c225e0-ce3e-11eb-82d0-0bde78b21518,interval:auto,query:(language:kuery,query:''),sort:!())",
-            "https://dfx1.my.connect.aws/metrics-reports",
-            "https://master.d3pfi7mlwkgahz.amplifyapp.com/"
+            "https://dfx1.my.connect.aws/real-time-metrics?reportArn=arn:aws:connect:us-east-1:153590104080:instance/2add78db-445c-4fbe-890e-a2cf0595de6e/report/bd7a1764-370f-4ff9-8cef-ee9c74a54be5"
         )
         $DealerFXChromeTwoURLs = [string]$DealerFXChromeTwoTabs
 
-        # Set up working tabs pertaining to tickets: GSheet "New Email - ALL OEM", GSheet "BLoc-notes des billets"
+        # Set up working tabs pertaining to tickets: GSheet "New Email - ALL OEM", GSheet "BLoc-notes des billets", AWS Agent App
         $DealerFXChromeThreeTabs = @(
             "https://docs.google.com/spreadsheets/d/1165JnOAgr0JkNRu7lIWUD_OmbwsSLWfDvx3Ha8JvSKk/",
-            "https://docs.google.com/spreadsheets/d/1q5NXZtm0Ln94gMi8FapfNvtbviVzrtcnS8IBUiH5Qic/"
+            "https://docs.google.com/spreadsheets/d/1q5NXZtm0Ln94gMi8FapfNvtbviVzrtcnS8IBUiH5Qic/",
+            "https://master.d3pfi7mlwkgahz.amplifyapp.com/"
         )
         $DealerFXChromeThreeURLs = [string]$DealerFXChromeThreeTabs
-        
+
         # Open all the programs for startup
         # Chrome - Dealer-FX standard working tabs for internal resources
         Start-Process -FilePath "C:\Program Files\Google\Chrome\Application\chrome.exe" -ArgumentList "--profile-directory=Default","--new-window",$DealerFXChromeOneURLs
@@ -50,8 +51,6 @@ do {
         Start-Process -FilePath "C:\Program Files\Google\Chrome\Application\chrome_proxy.exe" -ArgumentList "--profile-directory=Default","--app-id=ifgfkkbichmgomaifmnecbnpibfepmco"
         # Drive Chrome app
         Start-Process -FilePath "C:\Program Files\Google\Chrome\Application\chrome_proxy.exe"  -ArgumentList "--profile-directory=Default --app-id=aghbiahbpaijignceidepookljebhfak"
-        # Zoom
-        Start-Process -FilePath "C:\Program Files\Zoom\bin\Zoom.exe"
         # FortiClient VPN
         Start-Process -FilePath "C:\Program Files\Fortinet\FortiClient\FortiClient.exe"
 
