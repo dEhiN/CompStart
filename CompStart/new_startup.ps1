1da8a15fd91e318895853039ce87dc524c2737c2
David H. Watson <hiranwatson@gmail.com>
2023-07-19 22:35:55 -0400

    Copied files from feature_addon/json_file branch to main
    Copied files startup_data.json and test_data.json
    from feature_addon/json_file/data to data folder
    at project root level.
    
    Also copied script
    feature_addon/json_file/new_startup.ps1 to project
    root level.

---
+++ CompStart/new_startup.ps1
@@ -0,0 +1,117 @@
+# Script to automatically open the apps I want when the computer starts
+
+# Function to run a specific startup item
+# Input: 1. A String representing the full file path + program name with
+#        extension of the startup item
+#        2. A String representing the full arguments list to pass to
+#        this startup item when calling it
+#        3. A Int32 representing which startup item number this item is
+function Start-StartupItem {
+    param (
+        [Parameter(Mandatory)]
+        [int32]$StartItemNumber,
+        
+        [Parameter(Mandatory)]
+        [string]$ProgramPath,
+
+        [string]$ArgumentsList = $null
+    )
+    
+    if ($ArgumentsList) {
+        # TODO
+        #Write-Host $ArgumentsList
+        Start-Process -FilePath $ProgramPath -ArgumentList $ArgumentsList
+    }
+    else {
+        # TODO
+        #Write-Host $ArgumentsList
+        Start-Process -FilePath $ProgramPath
+    }
+}
+
+
+# Function to process all the data for a specific startup item
+# Input: 1. A PSCustomObject containing all the JSON data for a single
+#        startup item
+function Get-StarupItem {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [PSCustomObject]$StartupItem
+    )
+
+    # Grab each item's properties
+    $ItemNumber = $StartupItem.ItemNumber
+    $ItemPath = $StartupItem.FilePath
+    $ItemIsBrowser = $StartupItem.Browser
+    $ItemArgCount = $StartupItem.ArgumentCount
+    $ItemArgList = $StartupItem.ArgumentList
+    #$ItemComments = $StartupItem.Comments
+
+    # Process startup arguments
+    $LoopCounter = 0
+    $AllArgs = ""
+
+    if ($ItemArgCount -gt 0) {
+        foreach ($ItemArg in $ItemArgList) {
+            $LoopCounter += 1
+    
+            if ($ItemIsBrowser -and ($LoopCounter -eq $ItemArgCount)) {
+                $AllArgs += $ItemArg
+            }
+            else {
+                $AllArgs += [string]$ItemArg
+            }
+
+            $AllArgs += " "
+        }
+    }
+
+    Start-StartupItem -StartItemNumber $ItemNumber -ProgramPath $ItemPath -ArgumentsList $AllArgs
+    
+    #$DealerFXChromeOneTabs = @()
+    #$DealerFXChromeOneURLs = [string]$DealerFXChromeOneTabs
+    
+    #Start-Process -FilePath $ItemPath -ArgumentList
+
+    #Start-Process -FilePath "C:\Program Files\Google\Chrome\Application\chrome.exe" -ArgumentList "--profile-directory=Default","--new-window",$DealerFXChromeOneURLs
+}
+
+# Loop until user answers prompt
+$LoopTrue = $True
+do {
+    # Confirm if user wants to run script
+    #$UserPrompt = Read-Host -Prompt "Would you like to run this script [Y/N]"
+    $UserPrompt = "Y"
+
+    if (($UserPrompt -eq "Y") -or ($UserPrompt -eq "y")) {
+
+        # Tell loop to quit
+        $LoopTrue = $False
+
+        # Name and location of JSON file
+        $CurrentLocation = $PSScriptRoot
+        $DataFileLocation = "\data\"
+        #$DataFileName = "startup_data.json"
+        $DataFileName = "test_data.json"
+        $JSONFile = [string]$CurrentLocation + $DataFileLocation + $DataFileName
+
+        # Load JSON data
+        $JSONData = Get-Content -Path $JSONFile | ConvertFrom-Json
+        $StartupData = $JSONData.Items
+
+        # Loop through startup data array and process each item
+        foreach ($StartupItem in $StartupData) {
+            Get-StarupItem $StartupItem
+        }
+    }
+    elseif (($UserPrompt -eq "N") -or ($UserPrompt -eq "n")) {
+
+        # Tell loop to quit
+        $LoopTrue = $False
+
+        # Inform user of quitting script
+        Write-Host "Quitting script..."
+
+    }
+} while ($LoopTrue -eq $True)
