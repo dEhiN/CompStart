3ae119edc0b5a81330d9c27675ebf523d7af574c
David <hiranwatson@gmail.com>
2023-07-27 15:26:03 -0400

    Added code to create startup.bat
    Created variable to store the command to run the
    startup.ps1 PowerShell script. Then directed that
    variable to a file called test.bat. This is
    currently being used to not clash with
    startup.bat. However, in production, this will
    change to redirect to startup.bat.
    
    The idea is that when first "installing" this
    program, CompStart, there will only be setup.bat
    for less confusion. This batch file is run and it
    will create startup.bat, move it to the user's
    startup folder, and move all other program files
    to a permanent directory (most likely in Program
    Files).

--- CompStart/feature_addons/setup_startup/setup.bat
+++ CompStart/feature_addons/setup_startup/setup.bat
@@ -3,3 +3,10 @@ echo OFF
 :: Store path to user startup folder
 set START_PATH=\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
 set FULL_PATH="%HOMEDRIVE%%HOMEPATH%%START_PATH%"
+
+:: Store startup command to use when creating startup.bat
+set STARTUP_CMD=start powershell.exe -ExecutionPolicy Unrestricted -File %CD%\startup.ps1
+
+:: Create startup.bat which will be used to called the startup.ps1 PowerShell script
+echo %STARTUP_CMD% > test.bat
+REM NEED TO CHANGE THE NAME FROM test.bat TO startup.bat FOR PRODUCTION
