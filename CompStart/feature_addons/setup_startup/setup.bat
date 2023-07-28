22d2d61e18b826a4b69f543d008fe861a557297a
David <hiranwatson@gmail.com>
2023-07-28 13:08:05 -0400

    Updated the file location code for the startup.bat redirection
    Refactored the section for redirecting the
    startup command. Also renamed all the variables
    to make them clearer. Added in several echo
    commands to confirm each variable is holding the
    correct information but prior to committing, those
    commands were commented out.
    
    The following variables are now being used:
    
    STARTFOLDER_RELPATH: The relative path of the user
    start folder from after the user home directory
    STARTFOLDER_FULLPATH: The full path of the user
    start folder from the C drive
    BATCHSTART_CMD: The command to add to startup.bat
    which will run the startup.ps1 PowerShell script
    BATCHSTART_FILEPATH: The path of where to create
    startup.bat relative to %CD%
    BATCHSTART_FILENAME: The name startup.bat although
    currently it's set to test.bat until ready to
    put into production
    BATCHSTART_FILEPATHNAME: The full path plus name
    for the startup.bat file and it's really just the
    above two variables put together
    
    The redirection command is just echo
    BATCHSTART_CMD to BATCHSTART_FILEPATHNAME.
    Currently, while working on this feature branch,
    BATCHSTART_FILEPATH points to the branch folder
    but will need to be updated to point to the
    program root folder.

--- CompStart/feature_addons/setup_startup/setup.bat
+++ CompStart/feature_addons/setup_startup/setup.bat
@@ -1,12 +1,25 @@
 echo OFF
 
 :: Store path to user startup folder
-set START_PATH=\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
-set FULL_PATH="%HOMEDRIVE%%HOMEPATH%%START_PATH%"
+set STARTFOLDER_RELPATH=\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+set STARTFOLDER_FULLPATH="%HOMEDRIVE%%HOMEPATH%%STARTFOLDER_RELPATH%"
 
 :: Store startup command to use when creating startup.bat
-set STARTUP_CMD=start powershell.exe -ExecutionPolicy Unrestricted -File %CD%\startup.ps1
+set BATCHSTART_CMD=start powershell.exe -ExecutionPolicy Unrestricted -File %CD%\startup.ps1
+
+:: Store path and filename for startup.bat
+set BATCHSTART_FILEPATH=%CD%\CompStart\feature_addons\setup_startup\
+set BATCHSTART_FILENAME=test.bat
+set BATCHSTART_FILEPATHNAME=%BATCHSTART_FILEPATH%%BATCHSTART_FILENAME%
+:: NEED TO CHANGE BATCHSTART_FILENAME FROM test.bat TO startup.bat FOR PRODUCTION
+
+:: Testing to confirm variables are correct
+::echo STARTFOLDER_RELPATH: %STARTFOLDER_RELPATH%
+::echo STARTFOLDER_FULLPATH: %STARTFOLDER_FULLPATH%
+::echo BATCHSTART_FILEPATH: %BATCHSTART_FILEPATH%
+::echo BATCHSTART_CMD: %BATCHSTART_CMD%
+::echo BATCHSTART_FILENAME: %BATCHSTART_FILENAME%
+::echo BATCHSTART_FILEPATHNAME: %BATCHSTART_FILEPATHNAME%
 
 :: Create startup.bat which will be used to called the startup.ps1 PowerShell script
-echo %STARTUP_CMD% > test.bat
-REM NEED TO CHANGE THE NAME FROM test.bat TO startup.bat FOR PRODUCTION
+echo %BATCHSTART_CMD% > %BATCHSTART_FILEPATHNAME%
