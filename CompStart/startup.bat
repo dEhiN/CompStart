3782913f571e1c28c35d9fa29340e449eb0c8707
David <hiranwatson@gmail.com>
2023-08-01 14:42:10 -0400

    Updated file path in startup.bat to %CD%\CompStart\startup.ps1
    The previous path of just using %CD% was pointing
    to the root folder of the Dealer-FX folder. Had to
    add CompStart to specify the proper location of
    startup.ps1.

--- CompStart/startup.bat
+++ CompStart/startup.bat
@@ -1,3 +1,3 @@
 Rem %CD% returns the current directory
 
-start powershell.exe -ExecutionPolicy Unrestricted -File %CD%\startup.ps1
+start powershell.exe -ExecutionPolicy Unrestricted -File %CD%\CompStart\startup.ps1
