0f59cb08fc194746e04214eb150c2892d46eede3
David H. Watson <hiranwatson@gmail.com>
2023-08-02 00:15:10 -0400

    Updated batch file name in setup.bat
    Changed the BATCHSCRIPT_FILENAME variable from
    test.bat, which was used in testing, to
    startup.bat, which is being used in production.
    Also updated the BATCHSCRIPT_RELPATH variable to
    only "\CompStart\" since the
    feature_addons/setup_startup no longer exists.
    
    Ran the code and tested it worked. In the process,
    it updated startup.bat to have the full path to
    startup.ps1 for the "-File" parameter.

--- CompStart/startup.bat
+++ CompStart/startup.bat
@@ -1,3 +1,1 @@
-Rem %CD% returns the current directory
-
-start powershell.exe -ExecutionPolicy Unrestricted -File %CD%\CompStart\startup.ps1
+start powershell.exe -ExecutionPolicy Unrestricted -File c:\Users\David\DevProj\MyRepos\WorkProjects\Dealer-FX_Scripts\CompStart\startup.ps1 
