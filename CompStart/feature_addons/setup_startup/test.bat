e0892ef32f87ad29aaddcb2fd6717ec3ee24d4e3
David H. Watson <hiranwatson@gmail.com>
2023-08-01 23:44:14 -0400

    Refactored setup.bat to be more abstract
    Refactored setup.bat to use a variable called
    SCRIPT_ROOT to store the current directory. Added
    three new variables for the PowerShell script
    startup.ps1. Renamed the BATCHSTART variables to
    BATCHSCRIPT to have consistent naming.
    
    The added and modified variables are:
    
    SCRIPT_ROOT: The current directory the script is
    running from.
    
    BATCHSCRIPT_RELPATH: Replaces BATCHSTART_RELPATH
    BATCHSCRIPT_FILENAME: Replaces BATCHSTART_FILENAME
    BATCHSCRIPT_FULLPATH: Replaces
    BATCHSTART_FILENAMEPATH
    
    POWERSHELLSCRIPT_RELPATH: Relative path of the
    startup.ps1 script from SCRIPT_ROOT
    POWERSHELLSCRIPT_FILENAME: The name startup.ps1
    POWERSHELLSCRIPT_FULLPATH: The full path of
    startup.ps1, basically combining previous two
    variables
    
    BATCHSCRIPT_CMD: Same as before but updated to
    use POWERSHELLSCRIPT_FULLPATH for the "-File"
    parameter
    
    This should make it easier to update or change
    relative paths and filenames during testing and
    for production.

--- CompStart/feature_addons/setup_startup/test.bat
+++ CompStart/feature_addons/setup_startup/test.bat
@@ -1,1 +1,1 @@
-start powershell.exe -ExecutionPolicy Unrestricted -File c:\Users\david.watson\Documents\Scripts\Dealer-FX\startup.ps1 
+start powershell.exe -ExecutionPolicy Unrestricted -File c:\Users\David\DevProj\MyRepos\WorkProjects\Dealer-FX_Scripts\CompStart\startup.ps1 
