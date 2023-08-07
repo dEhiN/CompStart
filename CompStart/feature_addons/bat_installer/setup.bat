a4756efc83c646a2e7dab521485a4a121cbd52fd
David H. Watson <hiranwatson@gmail.com>
2023-08-06 22:14:11 -0400

    Changes to setup.bat

--- CompStart/feature_addons/bat_installer/setup.bat
+++ CompStart/feature_addons/bat_installer/setup.bat
@@ -13,16 +13,19 @@ set "POWERSHELLSCRIPT_FILENAME=startup.ps1"
 :: Filename for startup batch script
 :: Filename for startup JSON data file
 :: Path to user startup folder
+:: Path to user program or app folder
 if %PROD_ENV% == true (
     set "SCRIPT_ROOT=%CD%\CompStart\"
     set "BATCHSCRIPT_FILENAME=startup.bat"
     set "STARTJSON_FILENAME=startup_data.json"
     set "STARTFOLDER_FULLPATH=%HOMEDRIVE%%HOMEPATH%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
+    set "INSTALLFOLDER_RELPATH=%LOCALAPPDATA%\"
 ) else (
     set "SCRIPT_ROOT=%CD%\CompStart\feature_addons\bat_installer\"
     set "BATCHSCRIPT_FILENAME=CompStart.bat"
     set "STARTJSON_FILENAME=test_data.json"
-    set "STARTFOLDER_FULLPATH=%CD%\CompStart\feature_addons\bat_installer\startup_folder_test\"
+    set "STARTFOLDER_FULLPATH=%CD%\CompStart\feature_addons\bat_installer\simulation_tests\startup_folder\"
+    set "INSTALLFOLDER_RELPATH=simulation_tests\program_files_folder\"
 )
 
 :: Full path and name for startup batch script
@@ -37,4 +40,10 @@ set "BATCHSCRIPT_CMD=start powershell.exe -ExecutionPolicy Unrestricted -File %P
 :: Create startup batch script which will be used to called the startup PowerShell script
 echo %BATCHSCRIPT_CMD% > %BATCHSCRIPT_FULLPATH%
 
-cd %SCRIPT_ROOT%%SCRIPT_RELPATH%
+cd %SCRIPT_ROOT%%INSTALLFOLDER_RELPATH%
+
+if exists "%CD%\CompStart\nul" (
+    echo Already exists
+) else (
+    mkdir CompStart
+)
