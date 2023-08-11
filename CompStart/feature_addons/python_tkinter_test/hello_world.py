715aeb8bc5b7bd8bb4ed0e1078e3958bdb520900
David <hiranwatson@gmail.com>
2023-08-11 18:56:27 -0400

    Updated hello_world.py
    Added a second example to hello_world.py, another
    way to show how to use tkinter. Also added ability
    to choose between both examples.

--- CompStart/feature_addons/python_tkinter_test/hello_world.py
+++ CompStart/feature_addons/python_tkinter_test/hello_world.py
@@ -1,21 +1,70 @@
-# This example use of Tkinter is taken from
-# https://datatofish.com/executable-pyinstaller/
+import tkinter
+from tkinter.constants import RIDGE, BOTH, BOTTOM, X
 
-import tkinter as tk
+tk = tkinter.Tk()
 
-root = tk.Tk()
-
-canvas1 = tk.Canvas(root, width=300, height=300)
-canvas1.pack()
+# Example taken from https://datatofish.com/executable-pyinstaller/
 
 
-def hello():
-    label1 = tk.Label(root, text="Hello World!", fg='blue',
-                      font=('helvetica', 12, 'bold'))
-    canvas1.create_window(150, 200, window=label1)
+def program_one():
+    canvas = tkinter.Canvas(
+        tk,
+        width=300,
+        height=300
+    )
+    canvas.pack()
+
+    def program_one_hello():
+        label = tkinter.Label(
+            tk,
+            text="Hello World!",
+            fg='blue',
+            font=('helvetica', 12, 'bold')
+        )
+        canvas.create_window(150, 200, window=label)
+
+    button1 = tkinter.Button(
+        text="Click Me",
+        command=program_one_hello,
+        bg='brown',
+        fg='white'
+    )
+    canvas.create_window(150, 150, window=button1)
+
+    tk.mainloop()
+
+# Example taken from tooltip when hovering over the tkinter module name in VS
+# Code
 
 
-button1 = tk.Button(text="Click Me", command=hello, bg='brown', fg='white')
-canvas1.create_window(150, 150, window=button1)
+def program_two():
+    frame = tkinter.Frame(
+        tk,
+        relief=RIDGE,
+        borderwidth=2
+    )
+    frame.pack(fill=BOTH, expand=1)
 
-root.mainloop()
+    label = tkinter.Label(
+        frame,
+        text="Hello, World"
+    )
+    label.pack(fill=X, expand=1)
+
+    button = tkinter.Button(
+        frame,
+        text="Exit",
+        command=tk.destroy
+    )
+    button.pack(side=BOTTOM)
+
+    tk.mainloop()
+
+
+program_number = input("Do you want to run program 1 or program 2 (1/2)")
+if program_number == "1":
+    program_one()
+elif program_number == "2":
+    program_two()
+else:
+    print("Please run this again and enter only the numbers 1 or 2. Goodbye!")
