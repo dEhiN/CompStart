e9e46d09abb70382e38c177d44ffdc74ff6eb0bb
David H. Watson <hiranwatson@gmail.com>
2023-08-16 23:30:42 -0400

    Finished json_reader function in startupdata_modifier_tool.py
    Finished the function json_reader so it correctly
    either reads in the JSON data for a valid JSON
    file, or it returns an error message.
    
    Also added functionality to main that if an error
    message is returned, print that message and exit
    the program with a exit status of 1.
    
    Finally, had to remove the copy of test_data.json
    in the feature branch folder and use the original
    copy in data/json_data because part of the code to
    read the file involved using the os module along
    with os.path.join, os.getcwd and os.sep.

--- feature_addons/startupdata_modifier_tool/startupdata_editor.py
+++ feature_addons/startupdata_modifier_tool/startupdata_editor.py
@@ -2,9 +2,10 @@
 # file.
 
 import json
+import os
 
 
-def json_reader(path: str, file: str):
+def json_reader(json_path: list, json_filename: str):
     """Function to read in JSON data from a file
 
     Currently the function only validates if file:str in a valid JSON
@@ -13,24 +14,50 @@ def json_reader(path: str, file: str):
     that so we work with the JSON data in memory.
 
     Args:
-        path (str): A relative or absolute path to the folder with JSON file
-        file (str): The filename of the JSON file
+        json_path (list): A list containing the relative or absolute path to
+        the JSON file with each list item representing one subfolder from
+        Current Working Directory (CWD)
+        json_filename (str): The filename of the JSON file
     Returns:
-        string: Currently a message indicating the validity of file
+        bool: True if there is JSON data to return, false if not
+        string: An error message to display if there's no JSON data to return
+        or blank otherwise
+        dict: The actual JSON data if there is any to return or an empty
+        dictionary if not
     """
 
+    # Create return values
+    is_json_file = False
+    return_message = ""
+    json_data = {}
+
     # Validate if correct JSON file
-    split_file = file.split(".")
-    if len(split_file) == 1:
+    split_filename = json_filename.split(".")
+    if len(split_filename) == 1:
         return_message = "Please specify a valid JSON file name"
     else:
-        last_item = split_file[len(split_file) - 1]
-        if last_item == "json":
-            return_message = "Valid JSON file name"
+        last_item = split_filename[len(split_filename) - 1]
+        if last_item != "json":
+            return_message = f"Invalid JSON file name.\nReceived extension of: {last_item}\nExpected extension of: json"
         else:
-            return_message = f"Invalid JSON file name.\nExpected extension of: json\nReceived extension of: {last_item}"
+            is_json_file = True
 
-    return return_message
+    # Get the full path to the JSON file
+    full_path = os.getcwd()
+    for item in json_path:
+        full_path += os.sep + os.path.join(item)
+
+    # Add the full path and JSON filename together
+    json_file = full_path + os.sep + json_filename
+
+    # Read in JSON data
+    try:
+        with open(json_file, "r") as json_file:
+            json_data = json.load(json_file)
+    except:
+        pass
+
+    return is_json_file, return_message, json_data
 
 
 if __name__ == "__main__":
@@ -38,15 +65,17 @@ if __name__ == "__main__":
     is_prod = False
 
     # Variables for location and name of JSON file with startup data
-    json_path = ""
+    json_path = ["data", "json_data"]
     json_filename = ""
     if is_prod:
-        json_path = "/data/json_data/"
         json_filename = "startup_data.json"
     else:
-        json_path = "./"
         json_filename = "test_data.json"
 
-    # Read JSON data from file and print whether file is valid JSON file
-    status_message = json_reader(json_path, json_filename)
-    print(status_message)
+    # Check if given filename is a valid JSON file and if it is, return the
+    # JSON data, and if it's not, print an error message and exit with status
+    # of 1
+    is_json_file, status_message, json_data = json_reader(json_path, json_filename)
+    if not is_json_file:
+        print(status_message)
+        exit(1)
