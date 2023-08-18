a9075f9098c823f1c79ac777cc09d8161c43708d
David <hiranwatson@gmail.com>
2023-08-18 16:59:51 -0400

    Renamed JsonSchema enum class and created new enum class
    Renamed JsonSchema to JsonSchemaKeys because that
    enum class just contains the valid keys that can
    be used for startup_data.json.
    
    Create new enum class called JsonSchemaStructure
    with two members: OBJECT and ARRAY. OBJECT is a
    dictionary and ARRAY is a list. This can be used
    to generate the structure of the startup_data.json
    JSON data.

--- feature_addons/startupdata_modifier_tool/startupdata_editor.py
+++ feature_addons/startupdata_modifier_tool/startupdata_editor.py
@@ -20,7 +20,25 @@ EXAMPLE_JSON = {
 }
 
 
-class JsonSchema(Enum):
+class JsonSchemaStructure(Enum):
+    """Enum class based on startup_data.schema.json
+
+    This class will be used to create valid JSON data to use for the
+    startup_data.json file.
+
+    Args:
+        Enum: This class extends the Enum class from the enum module
+
+    Members:
+        An empty Python dictionary and list to correspond to a JSON object and
+        array
+    """
+
+    OBJECT = {}
+    ARRAY = []
+
+
+class JsonSchemaKeys(Enum):
     """Enum class based on startup_data.schema.json
 
     This class will be used to create valid JSON data to use for the
