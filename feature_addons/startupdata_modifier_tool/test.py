fb11bfa8ad67e1f0a0865dc33b98fb6230b98d8d
David H. Watson <hiranwatson@gmail.com>
2023-08-20 17:38:04 -0400

    Refactored code to make it more reusable in test.py
    Added a new global variable to contain the full
    absolute path to the test_data.json file.
    Refactored the code in the first unit test to
    use this global variable to compare against.

--- feature_addons/startupdata_modifier_tool/test.py
+++ feature_addons/startupdata_modifier_tool/test.py
@@ -5,23 +5,21 @@ import startupdata_editor as my_app
 # 'GLobal variables'
 JSON_PATH = ["feature_addons", "startupdata_modifier_tool"]
 JSON_FILENAME = "test_data.json"
+JSON_FILE = (
+    os.getcwd()
+    + os.sep
+    + "feature_addons"
+    + os.sep
+    + "startupdata_modifier_tool"
+    + os.sep
+    + "test_data.json"
+)
 
 
 def test_parse_full_path():
-    correct_results = (
-        os.getcwd()
-        + os.sep
-        + "feature_addons"
-        + os.sep
-        + "startupdata_modifier_tool"
-        + os.sep
-        + "test_data.json"
-    )
-
-    expected_message = "Expected: " + correct_results + "test_data.json"
-
+    expected_message = "Expected: " + JSON_FILE
     assert (
-        my_app.parse_full_path(JSON_PATH, JSON_FILENAME) == correct_results
+        my_app.parse_full_path(JSON_PATH, JSON_FILENAME) == JSON_FILE
     ), expected_message
 
 
