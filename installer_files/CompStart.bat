574d540536ba1bd71c47ec4499251f1d27997b2e
David <hiranwatson@gmail.com>
2023-08-23 19:04:27 -0400

    Deleted CompStart.bat in root

--- installer_files/CompStart.bat
+++
@@ -1,1 +0,0 @@
-start powershell.exe -ExecutionPolicy Unrestricted -File c:\Users\David\DevProj\MyRepos\WorkProjects\Dealer-FX_Scripts\CompStart\feature_addons\bat_installer\install_files\startup.ps1 
