ab033f7aa5cc9c4c8f558d79af7b4b760f9703e7
David H. Watson <hiranwatson@gmail.com>
2023-08-23 23:07:26 -0400

    Adjusted variables in install.ps1
    Removed variables $DataRelPath and $ScriptRelPath
    and created the variable $InstallRelPath which
    just points to the installer_files folder.
    
    The idea will be that everything needed for a
    first install will be in the installer_files
    folder.

--- feature_addons/powershell_installer/install.ps1
+++ feature_addons/powershell_installer/install.ps1
@@ -1,16 +1,4 @@
 # This will be the installation script used for CompStart
 
-# Convert this BAT code to PowerShell code
-<#
-:: Toggle between test and prod environments
-set "PROD_ENV=false"
-:: Relative path of install files folder
-set "SCRIPT_RELPATH=install_files\"
-:: Relative path of data folder
-set "DATA_RELPATH=install_files\data\"
-:: Name of startup PowerShell script
-set "POWERSHELLSCRIPT_FILENAME=startup.ps1"
-#>
 $ProdEnv = $false
-$ScriptRelPath = "installer_files\"
-$DataRelPath = "installer_files\data"
+$InstallRelPath = "installer_files\"
