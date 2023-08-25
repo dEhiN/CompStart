cc19cf853e95284ec4b71d39ab77287e02231beb
David <hiranwatson@gmail.com>
2023-08-25 15:42:27 -0400

    Changed directory structure of feature branch and created Python package/module structure
    Created subfolders called "program_files" and
    "test_files" from the feature branch root folder.
    Moved test.py to the test_files folder and
    everything else to the program_files folder.
    
    Also created blank __init__.py files in the
    feature branch root folder and the two subfolders
    so create a Python package and module structure.

---
+++ feature_addons/startup_data_modifier_tool/__init__.py
@@ -0,0 +1,3 @@
+# Python init file to make this folder a package
+# Full folder path:
+# CompStart/feature_addons/startup_data_modifier_tool
---
+++ feature_addons/startup_data_modifier_tool/test_files/__init__.py
@@ -0,0 +1,3 @@
+# Python init file to make this folder a module
+# Full folder path:
+# CompStart/feature_addons/startup_data_modifier_tool/test_files
