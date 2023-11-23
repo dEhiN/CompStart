d028ebca6e13f1faf3d3be9b3f82b5c41af9f47b
David <hiranwatson@gmail.com>
2023-11-23 16:11:37 -0500

    Used the changes made in commit c9e14d7 to fix an issue in validating the JSON data during the process to save a startup item
    While testing the functions
    data_validation_scenario and match_scenario in the
    module data_generate, the print statements
    modified and added in commit c9e14d7 revealed that
    the validation was failing when it shouldn't.
    
    Using the same print statements along with running
    the Python debugger in VS Code, I realized that in
    the module helper, in the function
    startup_data_validator, the file path being used
    wasn't complete because I assumed incorrectly what
    the current working directory is. That is now
    fixed.

--- feature_addons/startup_data_modifier_tool/dependencies/helper.py
+++ feature_addons/startup_data_modifier_tool/dependencies/helper.py
@@ -161,7 +161,7 @@ def startup_data_validator(json_data: dict, single_item: bool = False):
     schema_file = (
         "startup_item.schema.json" if single_item else "startup_data.schema.json"
     )
-    schema_path = ["config"]
+    schema_path = ["feature_addons", "startup_data_modifier_tool", "config"]
 
     results = app_demord.json_reader(schema_path, schema_file)
     read_status = results[0]
