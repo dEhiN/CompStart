105cfd67f56a29ee564c4412b58be52baf755860
Hiran Watson <hiranwatson@gmail.com>
2024-03-09 17:49:29 -0500

    Revert "Merge branch 'dEhiN/issue19'"
    This reverts commit db566a5763b59b6d37891c6714d1e79e12dbe2c5, reversing
    changes made to 544ce16f3c97fe97ebbe9b47aab823f771f6aa76.

--- releases/v1/m1/Demord/startup.bat
+++
@@ -1,7 +0,0 @@
-echo off
-set mypath=%~dp0
-set scriptname=startup.ps1
-set startupscript=%mypath%%scriptname%
-echo %startupscript%
-echo on
-start powershell.exe -ExecutionPolicy Unrestricted -File %startupscript% 
