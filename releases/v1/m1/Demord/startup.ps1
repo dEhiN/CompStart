dEhiN/CompStart

commit 038dad1e1a939030abac7c21829a3645aed0e99a

Updated the startup PowerShell script in the root folder
In commit eccc41b in the branch features/startup_data_modifier_tool, the startup data JSON structure was changed. Next, in commit 14466397, in the branch releases, the startup.ps1 file being used for release v1.1-alpha was refactored to acommodate the changes.

However, the script changes weren't made yet to the startup.ps1 file in the project root folder. Now, both startup.ps1 files have the same code with the changes from commit 14466397 in the branch releases.

startup.ps1
@@ -38,37 +38,21 @@ function Get-StarupItem {
 
     # Grab each item's properties
     $ItemNumber = $StartupItem.ItemNumber
-    $ItemName = $StartupItem.Name
     $ItemPath = $StartupItem.FilePath
-    $ItemDescription = $StartupItem.Description
-    $ItemIsBrowser = $StartupItem.Browser
     $ItemArgCount = $StartupItem.ArgumentCount
     $ItemArgList = $StartupItem.ArgumentList
 
     # Process startup arguments
-    $LoopCounter = 0
     $AllArgs = ""
 
     if ($ItemArgCount -gt 0) {
         foreach ($ItemArg in $ItemArgList) {
-            $LoopCounter += 1
-    
-            if ($ItemIsBrowser -and ($LoopCounter -eq $ItemArgCount)) {
-                $AllArgs += $ItemArg
-            }
-            else {
-                $AllArgs += [string]$ItemArg
-            }
-
-            $AllArgs += " "
+            $AllArgs += [string]$ItemArg + " "
         }
     }
 
     Start-StartupItem -StartItemNumber $ItemNumber -ProgramPath $ItemPath -ArgumentsList $AllArgs
 }
-
-# Setting to switch between testing and production - affects 2 spots in the do loop
-$IsProdEnv = $True
 
 # Loop until user answers prompt
 $LoopTrue = $True
@@ -89,14 +73,8 @@ do {
 
         # Name and location of JSON file
         $CurrentLocation = $PSScriptRoot
-        $DataFileLocation = "\data\json_data\"
-        $DataFileName = ""
-        if ($IsProdEnv) {
-            $DataFileName = "startup_data.json"
-        }
-        else {
-            $DataFileName = "test_data.json"
-        }
+        $DataFileLocation = "\config\"
+        $DataFileName = "startup_data.json"
         $JSONFile = [string]$CurrentLocation + $DataFileLocation + $DataFileName
 
         # Load JSON data

commit 363324033d03df1babfbe175671f1bff3fd53df8

Updated both startup PowerShell scripts to automatically work for both production and development environments
While testing things, realized that the changes made in commit 038dad1e caused the startup.ps1 file in the project root folder to not work properly since it was looking for a config folder.

Added an if-block to the startup script to check if the folder 'releases' is part of the script path by using the Test-Path cmdlet. Made the default folder path /config and then, if the cmdlet returns false, the script uses /data/json_data as the folder path.

Made these changes to the startup.ps1 file in the project root folder as well as the one in the /releases/v1/m1/Demord folder for release v1.1-alpha.

Finally, tested both scripts out using PowerShell and navigating to each folder respectively.

releases/v1/m1/Demord/startup.ps1
@@ -73,8 +73,17 @@ do {
 
         # Name and location of JSON file
         $CurrentLocation = $PSScriptRoot
+        # Set the location for production by default
         $DataFileLocation = "\config\"
+        # Check if this script is being run in production - as a release
+        # or if this script is being run in development
+        if (-not (Test-Path ($CurrentLocation + $DataFileLocation))) {
+            # Development environment, so change the location
+            $DataFileLocation = "\data\json_data\"
+        }
+        # Set the name of the JSON file
         $DataFileName = "startup_data.json"
+        # Concatenate all 3 variables to get the full script path
         $JSONFile = [string]$CurrentLocation + $DataFileLocation + $DataFileName
 
         # Load JSON data

startup.ps1
@@ -73,8 +73,17 @@ do {
 
         # Name and location of JSON file
         $CurrentLocation = $PSScriptRoot
+        # Set the location for production by default
         $DataFileLocation = "\config\"
+        # Check if this script is being run in production - as a release
+        # or if this script is being run in development
+        if (-not (Test-Path ($CurrentLocation + $DataFileLocation))) {
+            # Development environment, so change the location
+            $DataFileLocation = "\data\json_data\"
+        }
+        # Set the name of the JSON file
         $DataFileName = "startup_data.json"
+        # Concatenate all 3 variables to get the full script path
         $JSONFile = [string]$CurrentLocation + $DataFileLocation + $DataFileName
 
         # Load JSON data