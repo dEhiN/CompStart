49977f1e3904f745f86d58abbc333be2ab95c35f
David <hiranwatson@gmail.com>
2024-03-27 18:27:58 -0400

    Added some commented out code after the add_startup_item_arguments_list function for the next step in this issue
    In accordance with a comment made on the CompStart GitHub project page, added the function definitions and doc strings for the following 4 existing functions:
    
    edit_startup_item
    edit_startup_item_name
    edit_startup_item_description
    edit_startup_item_program_path
    
    Copied them directly from the startup_edit module and commented it all out. Also, added an explanatory doc string above the commented out code.

--- devenv/feature_addons/startup_data_modifier_tool/dependencies/startup_add.py
+++ devenv/feature_addons/startup_data_modifier_tool/dependencies/startup_add.py
@@ -98,3 +98,57 @@ def add_startup_item_arguments_list(arg_list: list = []):
                 print("Argument cannot be blank!")
 
     return new_arg_list
+
+
+"""
+The below copy-pasted function definitions and doc strings from the startup_edit module are to be 
+the template for creating the new functions in this module, startup_add.
+
+Specifically the functions need to be redone as follows:
+
+1. They each need to be renamed according to the following naming scheme:
+    edit_startup_item > add_startup_item
+    edit_startup_item_name > add_startup_item_name
+    edit_startup_item_description > add_startup_item_description
+    edit_startup_item_program_path > add_startup_item_program_path
+
+2. The function definition needs to be refactored namely for the parameters list but also for the 
+return variable (if need be)
+
+3. The doc string for each function will need to be updated
+"""
+# def edit_startup_item(orig_startup_item: dict, json_path: list, json_filename: str):
+#    """Helper function to edit a single startup item
+#    This function will take the specific startup item passed in, display it and allow the user to edit any part of that item. A copy of the startup item dictionary is created to work with and that copy is what's returned.
+#    Args:
+#        orig_startup_item (dict): A dictionary with the single startup item
+#        json_path (list): A list containing the relative or absolute path to the JSON file with each list item representing one subfolder from Current Working Directory (CWD)
+#        json_filename (str): The filename of the JSON file
+#    Returns:
+#        dict: The modified and updated startup item dictionary passed in.
+#    """
+
+# def edit_startup_item_name(item_name: str):
+#    """Helper function to change the name of a startup item
+#    Args:
+#        item_name (str): The existing startup item name
+#    Returns:
+#        str: The new startup item name
+#    """
+
+# def edit_startup_item_description(item_description: str):
+#    """Helper function to change the description of a startup item
+#    Args:
+#        item_description (str): The existing startup item description
+#    Returns:
+#        str: The new startup item description
+#    """
+
+# def edit_startup_item_program_path(item_name: str, item_path: str):
+#    """Helper function to change the path of a startup item
+#    Args:
+#        item_name (str): The existing startup item name
+#        item_path (str): The existing startup item absolute path
+#    Returns:
+#        str: The new startup item absolute path
+#    """
