b857024fe44d67da0e45ebcf6dd9d8293a6ffde2
Hiran Watson <hiranwatson@gmail.com>
2024-03-31 20:31:26 -0400

    Refactored the rest of the code to use the new user_menu_chooser function
    Updated the function declaration and docstring for the function edit_startup_item_arguments_list to not take the second parameter arg_count, since it's no longer needed. Also, updated the call from the function edit_startup_item to not pass in that paremeter.
    
    Within the function edit_startup_item_arguments_list, refactored things to grab the length of the arguments list passed in to track the argument count. Also, rewrote the menu sections, both for the first items menu, with the options to edit each argument, add an argument, or delete an argument, and the delete items menu, with the options to delete each existing argument.
    
    For both menus, removed any strings formatted to include a menu option number and all newlines, so the strings passed in for each menu option are in the correct format used by the new user_menu_chooser function. Also, removed any calls to user_menu_chooser that passed in a string, and made sure only a list of strings was passed in.
    
    Next, removed variables no longer needed such as menu_header and total_menu_choices. Also, fixed a call to the add_startup_item_arguments list when the user chooses to add an argument. The call was passing in the original argument list passed, not the local copy of the updated argument list. Now it passes in the local copy.
    
    Finally, tested things out. As part of the testing, adjusted the startup_data.json file and saved it, but then made a copy of the saved file called startup_data--issue25.json and put it in the testing folder. Also, as part of the testing, created a copy of startup.ps1 from /devenv/ to the root of the feature branch: /devenv/feature_addons/startup_data_modifier_tool. Modified the feature branch copy to look for startup_data.json in the same directory.
    
    This way, further testing of changes to the startup_data.json file can be checked to make sure the startup PowerShell script still works. In this case, the changes saved in startup_data--issue25.json were tested and opened correctly by the startup PowerShell script.

--- devenv/feature_addons/startup_data_modifier_tool/dependencies/startup_edit.py
+++ devenv/feature_addons/startup_data_modifier_tool/dependencies/startup_edit.py
@@ -68,7 +68,7 @@ def edit_startup_item(orig_startup_item: dict, json_path: list, json_filename: s
                 # Check if startup item has arguments
                 if arg_count > 0:
                     temp_arg_list = edit_startup_item_arguments_list(
-                        True, arg_count, startup_item[ENUM_JSK.ARGUMENTLIST.value]
+                        True, startup_item[ENUM_JSK.ARGUMENTLIST.value]
                     ).copy()
                 else:
                     temp_arg_list = edit_startup_item_arguments_list(False)
@@ -164,7 +164,7 @@ def edit_startup_item_program_path(item_name: str, item_path: str):
     return new_path
 
 
-def edit_startup_item_arguments_list(args_exist: bool, arg_count: int = 0, arg_list: list = []):
+def edit_startup_item_arguments_list(args_exist: bool, arg_list: list = []):
     """Helper function to change the arguments list of a startup item
 
     If there are no arguments, this function will ask the user if they want to add arguments. If
@@ -174,8 +174,6 @@ def edit_startup_item_arguments_list(args_exist: bool, arg_count: int = 0, arg_l
     Args:
         args_exist (bool): Required to let the function know if there are arguments.
 
-        arg_count (int, optional): The number of arguments. Defaults to 0.
-
         arg_list (list, optional): A list of the arguments. Defaults to [].
 
     Returns:
@@ -183,77 +181,55 @@ def edit_startup_item_arguments_list(args_exist: bool, arg_count: int = 0, arg_l
     """
     new_arg_list = arg_list.copy()
     new_argument = ""
+    arg_count = len(arg_list)
 
     # Check if there are existing arguments
     if args_exist and arg_count > 0:
-        # Initialize all the variables pertaining to the full user menu
-        menu_header = ""
-        menu_footer = ""
-        menu_choices = "[0] Return to previous menu"
-        add_choice, delete_choice, cancel_choice, total_menu_choices = (
-            0,
-            0,
-            0,
-            0,
-        )
-
         # Loop through the menu until the user cancels
         quit_loop = False
         while not quit_loop:
             # Create a menu listing all the arguments
-            arg_items_menu = []
+            item_menu = []
             for index in range(0, arg_count):
-                arg_items_menu.append(
-                    f"[{index + 1}] Edit argument {index + 1}: {new_arg_list[index]}\n"
-                )
+                item_menu.append(f"Edit argument {index + 1}: {new_arg_list[index]}")
 
             # Generate the full menu
             add_choice = arg_count + 1
             delete_choice = arg_count + 2
             cancel_choice = arg_count + 3
-            total_menu_choices = cancel_choice
-            menu_footer = (
-                f"[{add_choice}] Add a new argument\n"
-                + f"[{delete_choice}] Delete an argument\n"
-                + f"[{cancel_choice}] Return to the previous menu\n"
-            )
-            menu_choices = "".join(arg_items_menu) + menu_footer
+            menu_footer = [
+                "Add a new argument",
+                "Delete an argument",
+                "Return to the previous menu",
+            ]
+            item_menu.extend(menu_footer)
 
             # Find out what the user wants to do
-            user_choice = deps_chooser.user_menu_chooser(menu_choices, total_menu_choices, False)
+            user_choice = deps_chooser.user_menu_chooser(item_menu, False)
 
             # Depending on user choice, perform the next action
             if user_choice == cancel_choice:
                 # User chose to cancel out of this menu
                 quit_loop = True
             elif user_choice == add_choice:
-                pass
                 # User chose to add a new argument
-                new_arg_list = deps_item_add.add_startup_item_arguments_list(arg_list).copy()
+                new_arg_list = deps_item_add.add_startup_item_arguments_list(new_arg_list).copy()
                 arg_count = len(new_arg_list)
             elif user_choice == delete_choice:
                 # User chose to delete an existing argument
 
                 # Generate delete menu listing all the arguments
-                menu_header = "Please specify which argument you want to delete:\n"
-                arg_delete_menu = []
+                delete_menu = []
                 for index in range(0, arg_count):
-                    arg_delete_menu.append(
-                        f"[{index + 1}] Argument {index + 1}: {new_arg_list[index]}\n"
-                    )
+                    delete_menu.append(f"Remove argument {index + 1}: {new_arg_list[index]}")
                 cancel_choice = arg_count + 1
-                menu_footer = f"[{cancel_choice}] Cancel deletion\n"
-                menu_choices = menu_header + "".join(arg_delete_menu) + menu_footer
-                total_menu_choices = cancel_choice
-                user_choice = 0
+                delete_menu.append("Cancel deletion")
 
                 # Find out what the user wants to do
-                while user_choice == 0:
-                    user_choice = deps_chooser.user_menu_chooser(menu_choices, total_menu_choices)
+                user_choice = deps_chooser.user_menu_chooser(delete_menu, False)
 
                 if user_choice < cancel_choice:
                     # User chose a specific argument to delete
-
                     # Calculate which list index we're working with
                     changed_argument_index = user_choice - 1
                     try:
