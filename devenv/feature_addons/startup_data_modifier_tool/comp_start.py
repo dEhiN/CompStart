92083283e091821e666355bbe92bd2acce563065
David <hiranwatson@gmail.com>
2024-10-28 14:02:56 -0400

    Refactored the function new_file_chooser
    In the module chooser, refactored the new_file_chooser function. Changed the two boolean variables from is_default and to_continue to is_default and create_file. Updated the docstring to explain what a value of True means for each variable and what False means. Reworded the third menu choice to specify that returning to the main menu means skipping creating a startup file. Finally, refactored the if block as necessary.
    
    Also made the necessary variable name changes in comp_start to stay in line with the new variable names in new_file_chooser.

--- devenv/feature_addons/startup_data_modifier_tool/comp_start.py
+++ devenv/feature_addons/startup_data_modifier_tool/comp_start.py
@@ -53,9 +53,9 @@ if __name__ == "__main__":
                 deps_helper.program_info()
             case 2:
                 # Find out which type of new file the user wants
-                is_default, to_continue = deps_chooser.new_file_chooser()
+                is_default, create_file = deps_chooser.new_file_chooser()
 
-                if to_continue:
+                if create_file:
                     # Create a new JSON file
                     status_state, status_message = deps_json.json_creator(
                         json_path, json_filename, is_default
