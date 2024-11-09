560bf4988e89f25fb175b2586ec4ff3356ec31f8
Hiran Watson <hiranwatson@gmail.com>
2024-11-09 17:26:38 -0500

    Comment changes
    While creating the README for the experimental_content folder, both Python scripts in the tkinter/tkinter_examples folder were reviewed to recall what they did.
    
    While reviewing, realized that the comments for the hello_world.py file was a little confusing. Redid the comments to have one fully explanatory comment at the beginning of the script.

--- devenv/experimental_content/tkinter/tkinter_examples/hello_world.py
+++ devenv/experimental_content/tkinter/tkinter_examples/hello_world.py
@@ -1,61 +1,37 @@
+# This is an example demonstrating the use of TKinter dialog boxes. The code for program one was taken from https://datatofish.com/executable-pyinstaller/. The code for program two was taken from the tooltip that shows when hovering over the tkinter module name in VS Code. Both programs were added to this one file and the code was extended to show two different options for what is displayed in the dialog box via program one or two.
+
 import tkinter
 from tkinter.constants import RIDGE, BOTH, BOTTOM, X
 
 tk = tkinter.Tk()
 
-# Example taken from https://datatofish.com/executable-pyinstaller/
-
 
 def program_one():
-    canvas = tkinter.Canvas(
-        tk,
-        width=300,
-        height=300
-    )
+    canvas = tkinter.Canvas(tk, width=300, height=300)
     canvas.pack()
 
     def program_one_hello():
         label = tkinter.Label(
-            tk,
-            text="Hello World!",
-            fg='blue',
-            font=('helvetica', 12, 'bold')
+            tk, text="Hello World!", fg="blue", font=("helvetica", 12, "bold")
         )
         canvas.create_window(150, 200, window=label)
 
     button1 = tkinter.Button(
-        text="Click Me",
-        command=program_one_hello,
-        bg='brown',
-        fg='white'
+        text="Click Me", command=program_one_hello, bg="brown", fg="white"
     )
     canvas.create_window(150, 150, window=button1)
 
     tk.mainloop()
 
-# Example taken from tooltip when hovering over the tkinter module name in VS
-# Code
-
 
 def program_two():
-    frame = tkinter.Frame(
-        tk,
-        relief=RIDGE,
-        borderwidth=2
-    )
+    frame = tkinter.Frame(tk, relief=RIDGE, borderwidth=2)
     frame.pack(fill=BOTH, expand=1)
 
-    label = tkinter.Label(
-        frame,
-        text="Hello, World"
-    )
+    label = tkinter.Label(frame, text="Hello, World")
     label.pack(fill=X, expand=1)
 
-    button = tkinter.Button(
-        frame,
-        text="Exit",
-        command=tk.quit
-    )
+    button = tkinter.Button(frame, text="Exit", command=tk.quit)
     button.pack(side=BOTTOM)
 
     tk.mainloop()
