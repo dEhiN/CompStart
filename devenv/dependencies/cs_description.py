e9f935b655de95c35001c7843183cc4fa7bb53a2
David <hiranwatson@gmail.com>
2024-11-21 16:49:07 -0500

    Added a new dependency file
    Created a new Python file called cs_description.py in the dependencies folder. This file will hold a global string variable that will be the actual description or explanation of what CompStart is and how it works.
    
    When option 1 is selected from the main menu, this variable will be used for the output.

---
+++ devenv/dependencies/cs_description.py
@@ -0,0 +1,14 @@
+# File to store the description of the CompStart program for use when the user selects option 1 from the main menu
+
+ CS_DESCRIPTION =
+"""
+What is CompStart?
+
+CompStart is a computer startup tool designed to make your life easier.
+
+What does that mean?
+
+Have you ever wished to be able to log into your laptop at work and have all of your programs automatically start up? Or, maybe you are working on some personal project at home and don't want to be reopening the file or program you're using every time you turn on your computer.
+
+With CompStart, you can have any program automatically open when you log in, have your favourite browser open to any website you desire, as well as open any file you want, such as a Word document.
+"""
