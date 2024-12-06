c5eb689cd7db7c49f30e2ac1d62e1f8a3714761e
David H. Watson <hiranwatson@gmail.com>
2024-12-06 07:31:40 -0500

    Created the initial PowerShell script
    Called it PTGenerator.ps1 and added an intro comment describing what it does. Per the comment:
    
    This script is meant to be used for releases and will assume the release folder has been created. As a result, only a "py-tool" folder will be created where everything will be copied and worked on.

---
+++ devenv/features/generate_python_executable/PTGenerator.ps1
@@ -0,0 +1,1 @@
+# PowerShell script to automate the generation of the Python command line tool CompStart.py to an executable using the Python module PyInstaller. This script is meant to be used for releases and will assume the release folder has been created. As a result, only a "py-tool" folder will be created where everything will be copied and worked on.
