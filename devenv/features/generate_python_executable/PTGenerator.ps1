ebb1faea6079b87119542fc9fe5ae4cdc5d37508
David H. Watson <hiranwatson@gmail.com>
2024-12-09 17:14:20 -0500

    Added more to PSGenerator.ps1
    Added to the script to get the full releases path by asking the user for the major, then minor version numbers, and then for any subfolders - such as 1.1-alpha.

--- devenv/features/generate_python_executable/PTGenerator.ps1
+++ devenv/features/generate_python_executable/PTGenerator.ps1
@@ -15,7 +15,23 @@ if (-Not (Select-String -InputObject $CurrentDirectory -Pattern "CompStart" -Cas
 }
 
 # Create the paths to be used in the script
-$ReleasesPath = "$CurrentDirectory\$ReleasesFolder\"
-$DevPath = "$CurrentDirectory\$DevFolder\"
-$DependenciesPath = "$DevPath\$DependenciesFolder\"
+$ReleasesPath = "$CurrentDirectory\$ReleasesFolder"
+$DevPath = "$CurrentDirectory\$DevFolder"
+$DependenciesPath = "$DevPath\$DependenciesFolder"
+
+# Determine which version number we are working with
+Write-Host "What is the release major version number? " -NoNewline
+$ReleaseMajorVersion = $Host.UI.ReadLine()
+
+Write-Host "What is the release minor version number? " -NoNewline
+$ReleaseMinorVersion = $Host.UI.ReadLine()
+
+Write-Host "Please enter a subfolder under v$ReleaseMajorVersion\m$ReleaseMinorVersion or leave blank if there is none: " -NoNewline
+$Subfolder = $Host.UI.ReadLine()
+
+$FullReleasesPath = "$ReleasesPath\v$ReleaseMajorVersion\m$ReleaseMinorVersion"
+
+if ($Subfolder -ne "") {
+    $FullReleasesPath += "\$Subfolder"
+}
 
