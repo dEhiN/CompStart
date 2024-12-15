814e49b1a4e4270702b1082cba7a11d388bdd847
David H. Watson <hiranwatson@gmail.com>
2024-12-14 23:16:49 -0500

    Added checks for the release major and minor folders
    Created new variables and renamed one variable to allow for the breakup of the path between the major version folder, the minor version folder, and the actual release folder.
    
    Added the same if-block checks to see if the major and minor version folders exist.
    
    Tested it out with multiple scenarios and the script correctly outputs when one of the folders doesn't exist.

--- devenv/features/create_new_release/CreateRelease.ps1
+++ devenv/features/create_new_release/CreateRelease.ps1
@@ -13,7 +13,7 @@ if (-Not (Select-String -InputObject $ProjectRootPath -Pattern "CompStart" -Case
 # Initialize the variables to be used in the script
 $ReleasesFolder = "releases"
 $ReleaseVersionsFolder = "release-versions"
-$ReleasesVersionsPath = "$ProjectRootPath\$ReleasesFolder\$ReleaseVersionsFolder"
+$ReleaseVersionsPath = "$ProjectRootPath\$ReleasesFolder\$ReleaseVersionsFolder"
 
 # Determine which release version to create
 Write-Host "What is the release major version number? " -NoNewline
@@ -25,17 +25,27 @@ $ReleaseMinorVersion = $Host.UI.ReadLine()
 Write-Host "`nWhat is the release tag for v$ReleaseMajorVersion.$ReleaseMinorVersion (or leave blank if there is none)? " -NoNewline
 $ReleaseTag = $Host.UI.ReadLine()
 
-# Determine the full path to the release directory we are working with
-$FullReleasesPath = "$ReleasesVersionsPath\v$ReleaseMajorVersion\m$ReleaseMinorVersion\$ReleaseMajorVersion.$ReleaseMinorVersion"
+# Determine the release subfolders
+$ReleaseMajorPath = "$ReleaseVersionsPath\v$ReleaseMajorVersion"
+$ReleaseMinorPath = "$ReleaseMajorPath\m$ReleaseMinorVersion"
+$ReleaseFullPath = "$ReleaseMinorPath\$ReleaseMajorVersion.$ReleaseMinorVersion"
 
 # Add the release tag if one exists
 if ($ReleaseTag -ne "") {
-    $FullReleasesPath += "-$ReleaseTag"
+    $ReleaseFullPath += "-$ReleaseTag"
 }
 
-# Before proceeding, confirm the path exists and if not, try to create it
-if (-Not (Test-Path $FullReleasesPath)) {
-    Write-Host "`nThe path $FullReleasesPath doesn't exist."
+# Before proceeding, confirm the paths exist and if not, try to create them
+if (-Not (Test-Path $ReleaseMajorPath)) {
+    Write-Host "`nThe path $ReleaseMajorPath doesn't exist."
+}
+
+if (-Not (Test-Path $ReleaseMinorPath)) {
+    Write-Host "`nThe path $ReleaseMinorPath doesn't exist."
+}
+
+if (-Not (Test-Path $ReleaseFullPath)) {
+    Write-Host "`nThe path $ReleaseFullPath doesn't exist."
 
     # Loop until user answers prompt
     $LoopTrue = $True
@@ -59,7 +69,7 @@ if (-Not (Test-Path $FullReleasesPath)) {
     } while ($LoopTrue -eq $True)
 
     # Since loop ended, user chose to create the release directory
-    Write-Host "`nCreating directory $FullReleasesPath..."
+    Write-Host "`nCreating directory $ReleaseFullPath..."
     Start-Sleep -Seconds 1
-    New-Item $FullReleasesPath -ItemType Directory > $null
+    New-Item $ReleaseFullPath -ItemType Directory > $null
 }
