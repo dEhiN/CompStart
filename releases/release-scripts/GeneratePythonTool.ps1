9e9f13371af8eca6aa9bb93623a5ddcffeb35d15
David H. Watson <hiranwatson@gmail.com>
2024-12-22 14:58:56 -0500

    Updated all 3 release automation scripts
    Added calls for the scripts CreateReleaseFolder.ps1, GeneratePythonTools.ps1, and CopyReleaseContent.ps1 to the new SetStartDirectory.psm1 module.
    
    Used the call to set the start directory to the proper project root and quit if that wasn't possible.

--- releases/release-scripts/GeneratePythonTool.ps1
+++ releases/release-scripts/GeneratePythonTool.ps1
@@ -3,8 +3,14 @@
 # Get the location of the release folder root
 $ProjectRootPath = Get-Location
 
+# Import the Set-StartDirectory function
+Import-Module ".\SetStartDirectory.psm1"
+
+# Set the starting directory to the project root
+$SetCSSuccess = Set-StartDirectory "CompStart"
+
 # Check to make sure we are in the project root
-if (-Not (Select-String -InputObject $ProjectRootPath -Pattern "CompStart" -CaseSensitive)) {
+if (-Not $SetCSSuccess) {
     # Inform user project root can't be found and the script is ending
     Write-Host "`nUnable to find project root. Quitting script..."
     Exit
