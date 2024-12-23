84c9d4f0716ea6361d3231e1a1d12f60e0072da5
David H. Watson <hiranwatson@gmail.com>
2024-12-23 18:38:46 -0500

    Added to the GenerateReleasePackage.ps1 script
    Partially with the help of GitHub AI and partially by copying from the release automation scripts, wrote up the code for the following tasks:
    
    - get the release major version, minor version, and release tag from the user
    - check for the existence of the release folder
    - check for the existence of the CompStart folder, release-notes folder, and instructions.txt file in the release folder
    - check for the existence of the packages folder
    
    In all 3 cases, the user is alerted if the checks fail. However, for the last check, this needs to change to have the script create the package folder.

--- devenv/features/create_release_package/GenerateReleasePackage.ps1
+++ devenv/features/create_release_package/GenerateReleasePackage.ps1
@@ -15,3 +15,67 @@ if (-Not $SetCSSuccess) {
     Write-Host "`nUnable to find project root. Quitting script..."
     Exit
 }
+
+# Initialize the static variables to be used in the script
+$PackagesFolder = "packages"
+$ReleasesFolder = "releases"
+$ReleaseVersionsFolder = "release-versions"
+$CompStartFolder = "CompStart"
+$ReleaseNotesFolder = "release-notes"
+$ReleaseInstructionsFile = "instructions.txt"
+$ReleasesPath = "$ProjectRootPath\$ReleasesFolder"
+$PackagesPath = "$ProjectRootPath\$PackagesFolder"
+$ReleaseVersionsPath = "$ReleasesPath\$ReleaseVersionsFolder"
+
+
+# Determine which release version to work with
+Write-Host "`nWhat is the release major version number? " -NoNewline
+$ReleaseMajorVersion = $Host.UI.ReadLine()
+
+Write-Host "What is the release minor version number? " -NoNewline
+$ReleaseMinorVersion = $Host.UI.ReadLine()
+
+Write-Host "What is the release tag for v$ReleaseMajorVersion.$ReleaseMinorVersion (or leave blank if there is none)? " -NoNewline
+$ReleaseTag = $Host.UI.ReadLine()
+
+# Create the full release version for later
+$ReleaseFullVersion = "$ReleaseMajorVersion.$ReleaseMinorVersion"
+
+# Add the release tag if one exists
+if ($ReleaseTag -ne "") {
+    $ReleaseFullVersion += "-$ReleaseTag"
+}
+
+# Store the release and package subfolder paths
+$ReleaseMajorPath = "$ReleaseVersionsPath\v$ReleaseMajorVersion"
+$PackageMajorPath = "$PackagesPath\v$ReleaseMajorVersion"
+$ReleaseMinorPath = "$ReleaseMajorPath\m$ReleaseMinorVersion"
+$PackageMinorPath = "$PackageMajorPath\m$ReleaseMinorVersion"
+$ReleaseFullPath = "$ReleaseMinorPath\$ReleaseFullVersion"
+$PackageFullPath = "$PackageMinorPath\$ReleaseFullVersion"
+
+# Before proceeding, confirm the release folder path exists and if not, alert the user to create it
+if (-Not (Test-Path $ReleaseFullPath)) {
+    Write-Host "`nThe release folder $ReleaseFullPath does not exist!`nPlease create the release folder before running this script..."
+    Exit
+}
+
+$ReleaseCSFolder = "$ReleaseFullPath\$CompStartFolder"
+$ReleaseNotesFolder = "$ReleaseFullPath\$ReleaseNotesFolder"
+$ReleaseInstructionsPath = "$ReleaseFullPath\$ReleaseInstructionsFile"
+
+# Check if the release folder has the necessary folders and files
+if (-Not (Test-Path $ReleaseCSFolder) -Or -Not (Test-Path $ReleaseNotesFolder) -Or -Not (Test-Path $ReleaseInstructionsPath)) {
+    Write-Host "`nThe release folder $ReleaseFullPath is missing necessary folders and files!`nPlease ensure the release folder has the following folders and files:`n- $CompStartFolder`n- $ReleaseNotesFolder`n- $ReleaseInstructionsFile`n"
+    Exit
+}
+
+# Also check if the packages folder exists
+if (-Not (Test-Path $PackageFullPath)) {
+    Write-Host "`nThe packages folder $PackageFullPath does not exist!`nPlease create the packages folder before running this script..."
+    Exit
+}
+
+# Get the release package name
+$ReleasePackageName = "CompStart-$ReleaseFullVersion.zip"
+
