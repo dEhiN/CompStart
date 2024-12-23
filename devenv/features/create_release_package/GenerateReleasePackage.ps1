cfd52f7f2fcf64d4a09fb5e284fcf860554dabe5
David H. Watson <hiranwatson@gmail.com>
2024-12-23 18:10:15 -0500

    Copied over the Python module to set the start directory to the feature branch
    Copied over Set-StartDirectory.psm1 from /releases to /devenv/features/create_release_package.
    
    Also copied over the first few lines of code from the release automation scripts to the GenerateReleasePackage.ps1 script to correctly set the starting location to the project root.

--- devenv/features/create_release_package/GenerateReleasePackage.ps1
+++ devenv/features/create_release_package/GenerateReleasePackage.ps1
@@ -1,1 +1,17 @@
 # This script is for automating the task of creating a release package, or in other words, a package artifact for a specific release.
+
+# Import the Set-StartDirectory function
+Import-Module ".\Set-StartDirectory.psm1"
+
+# Set the starting directory to the project root
+$SetCSSuccess = Set-StartDirectory "CompStart"
+
+# Get the location of the release folder root
+$ProjectRootPath = Get-Location
+
+# Check to make sure we are in the project root
+if (-Not $SetCSSuccess) {
+    # Inform user project root can't be found and the script is ending
+    Write-Host "`nUnable to find project root. Quitting script..."
+    Exit
+}
