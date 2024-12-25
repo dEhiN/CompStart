f883651acf1205fc6e6ea8ab76f8922040a64851
David H. Watson <hiranwatson@gmail.com>
2024-12-25 10:43:25 -0500

    Adjusted the import of the Set-StartDirectory module
    After testing out the different ways PowerShell provides location info, changed the Import-Module statement for the Set-StartDirectory module to use $PSScriptRoot as the starting point.
    
    This way, it doesn't matter where the "current location" is since currently the module is in the same directory as the GenerateReleasePackage.ps1 script.

--- devenv/features/create_release_package/GenerateReleasePackage.ps1
+++ devenv/features/create_release_package/GenerateReleasePackage.ps1
@@ -1,7 +1,7 @@
 # This script is for automating the task of creating a release package, or in other words, a package artifact for a specific release.
 
 # Import the Set-StartDirectory function
-Import-Module ".\Set-StartDirectory.psm1"
+Import-Module "$PSScriptRoot\Set-StartDirectory.psm1"
 
 # Set the starting directory to the project root
 $SetCSSuccess = Set-StartDirectory "CompStart"
