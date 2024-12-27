58d5ef8a13f6bab92645be498e4453f1f273f695
Hiran Watson <hiranwatson@gmail.com>
2024-12-26 19:03:43 -0500

    Minor changes to install.ps1
    Made some minor changes to the existing code for install.ps1. Mainly, the changes were adding comments and using [System.IO.Path]::DirectorySeparatorChar for path separators.

--- devenv/features/cs-installer/powershell-installer/install.ps1
+++ devenv/features/cs-installer/powershell-installer/install.ps1
@@ -1,17 +1,18 @@
 # This will be the installation script used for CompStart
 
+# Check if the current environment is production or not
 $IsProdEnv = $false
-$InstallRelPath = "installer_files\"
 
 if ($IsProdEnv) {
     Write-Host "Production isn't ready yet. Try again later..."
-    exit
+    Exit
 }
 
-$InstallFullPath = ($PSScriptRoot + "\" + $InstallRelPath)
-
+# Get a list of all the files to "install"
+$InstallRelPath = "installer-files"
+$SplitChar = [System.IO.Path]::DirectorySeparatorChar
+$InstallFullPath = ($PSScriptRoot + $SplitChar + $InstallRelPath)
 $FilesList = Get-ChildItem -Recurse $InstallFullPath
-
 foreach ($Item in $FilesList) {
     Write-Host $Item.FullName
 }
