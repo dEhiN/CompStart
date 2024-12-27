7031406a6d7537eb0b8d4f4f68161bc5d5d035a8
Hiran Watson <hiranwatson@gmail.com>
2024-12-26 19:35:13 -0500

    Added a function to create the CompStart folder
    Added a function New-CSFolder that will create the CompStart folder. It takes an optional string parameter that would function as the parent path for the folder. If it's not supplied, or the supplied parameter isn't a valid path, the folder is created at %LocalAppData%.
    
    Also created 2 global variables. Made the directory separator variable a global one and add a global sleep timer variable.

--- devenv/features/cs-installer/powershell-installer/install.ps1
+++ devenv/features/cs-installer/powershell-installer/install.ps1
@@ -1,5 +1,43 @@
 # This will be the installation script used for CompStart
 
+# GLobal variables
+$Global:SplitChar = [System.IO.Path]::DirectorySeparatorChar
+$Global:SleepTime = 2
+
+function New-CSFolder {
+    [CmdletBinding()]
+    param (
+        [string] $SuppliedPath
+    )
+
+    # Initialize function variables
+    $CSFolder = "CompStart"
+    $CSParentPath = [System.Environment]::GetFolderPath('LocalApplicationData')
+
+    # Test the passed in parameter and use it if it's a valid path
+    if ($SuppliedPath) {
+        if (-Not (Test-Path -Path $SuppliedPath)) {
+            Write-Host "$SuppliedPath is not a valid file system path...defaulting to $CSParentPath..."
+        }
+        else {
+            $CSParentPath = $SuppliedPath
+        }
+    }
+
+    # Set the full CompStart folder path
+    $CSFullPath = $CSParentPath + $global:SplitChar + $CSFolder
+
+    # Create the folder if need be
+    if (-Not (Test-Path $CSFullPath)) {
+        Write-Host "`nCreating CompStart folder..."
+        Start-Sleep $Global:SleepTime
+        New-Item -Path $CSFullPath -ItemType "Directory" > $null
+        Write-Host "...folder successfully created at $CSFullPath"
+    }
+
+    
+}
+
 # Check if the current environment is production or not
 $IsProdEnv = $false
 
@@ -10,9 +48,10 @@ if ($IsProdEnv) {
 
 # Get a list of all the files to "install"
 $InstallRelPath = "installer-files"
-$SplitChar = [System.IO.Path]::DirectorySeparatorChar
-$InstallFullPath = ($PSScriptRoot + $SplitChar + $InstallRelPath)
+$InstallFullPath = ($PSScriptRoot + $global:SplitChar + $InstallRelPath)
 $FilesList = Get-ChildItem -Recurse $InstallFullPath
 foreach ($Item in $FilesList) {
     Write-Host $Item.FullName
 }
+
+New-CSFolder -SuppliedPath "C:\Users\David\AppData\Local\Programs"
