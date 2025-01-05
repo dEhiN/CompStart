7d8a68e5534d8d5eb518f335f47adfe0e8eb1ccd
Hiran Watson <hiranwatson@gmail.com>
2025-01-05 00:25:23 -0500

    Copied over the relevant code from the previous automation scripts
    Copied over all potentially relevant code from CreateReleaseFolder.ps1, GeneratePythonTool.ps1, CopyReleaseContent.ps1, and GenerateReleasePackage.ps1 to the DeployRelease.ps1 script.
    
    Enclosed each copied text in PowerShell multi-line comment blocks <#..#> so the code doesn't execute or cause problems but is available to work through.

--- prodenv/scripts/DeployRelease.ps1
+++ prodenv/scripts/DeployRelease.ps1
@@ -399,3 +399,374 @@ function Start-Release {
     # Deal with the release folder
     Set-ReleasePath -ReleaseVersion $ReleaseVersion
 }
+
+# The following code has been copied from the CreateReleaseFolder script:
+<#
+# Set the starting directory to the project root
+$SetCSSuccess = Set-StartDirectory "CompStart"
+
+# Get the location of the release folder root
+$ProjectRootPath = Get-Location
+
+# Check to make sure we are in the project root
+if (-Not $SetCSSuccess) {
+    # Inform user project root can't be found and the script is ending
+    Write-Host "`nUnable to find project root. Quitting script..."
+    Exit
+}
+
+# Initialize the variables to be used in the script
+$ReleasesFolder = "releases"
+$ReleaseVersionsFolder = "versions"
+$ReleaseVersionsPath = "$ProjectRootPath\$ReleasesFolder\$ReleaseVersionsFolder"
+
+# Determine which release version to create
+Write-Host "`nWhat is the release major version number? " -NoNewline
+$ReleaseMajorVersion = $Host.UI.ReadLine()
+
+Write-Host "What is the release minor version number? " -NoNewline
+$ReleaseMinorVersion = $Host.UI.ReadLine()
+
+Write-Host "What is the release tag for v$ReleaseMajorVersion.$ReleaseMinorVersion (or leave blank if there is none)? " -NoNewline
+$ReleaseTag = $Host.UI.ReadLine()
+
+# Create the full release version for later
+$ReleaseFullVersion = "$ReleaseMajorVersion.$ReleaseMinorVersion"
+
+# Add the release tag if one exists
+if ($ReleaseTag -ne "") {
+    $ReleaseFullVersion += "-$ReleaseTag"
+}
+
+# Store the release subfolder paths
+$ReleaseMajorPath = "$ReleaseVersionsPath\v$ReleaseMajorVersion"
+$ReleaseMinorPath = "$ReleaseMajorPath\m$ReleaseMinorVersion"
+$ReleaseFullPath = "$ReleaseMinorPath\$ReleaseFullVersion"
+#>
+
+# The following code has been copied from the GeneratePythonTool script:
+<#
+# Initialize the relevant folder and file variables to be used in the script
+$ReleasesFolder = "releases"
+$ReleaseVersionsFolder = "versions"
+$DevFolder = "devenv"
+$DependenciesFolder = "dependencies"
+$PyToolsFolder = "py-tools"
+$CSScript = "CompStart.py"
+$ReleaseFullVersion = ""
+
+# Create the paths to be used in the script
+$ReleasesPath = "$ProjectRootPath\$ReleasesFolder"
+$ReleaseVersionsPath = "$ReleasesPath\$ReleaseVersionsFolder"
+$DevPath = "$ProjectRootPath\$DevFolder"
+$CSPath = "$DevPath\$CSScript"
+$DependenciesPath = "$DevPath\$DependenciesFolder"
+
+# Initialize the pyinstaller specific variables
+$PyIFilePath = "pyinstaller"
+$PyIArgumentArray = @(
+    $CSPath,
+    "--onefile"
+)
+
+# Determine which version number we are working with
+Write-Host "`nWhat is the release major version number? " -NoNewline
+$ReleaseMajorVersion = $Host.UI.ReadLine()
+
+Write-Host "What is the release minor version number? " -NoNewline
+$ReleaseMinorVersion = $Host.UI.ReadLine()
+
+Write-Host "What is the release tag for v$ReleaseMajorVersion.$ReleaseMinorVersion (or leave blank if there is none)? " -NoNewline
+$ReleaseTag = $Host.UI.ReadLine()
+
+$ReleaseFullVersion = "$ReleaseMajorVersion.$ReleaseMinorVersion"
+
+# Add the release tag if one exists
+if ($ReleaseTag -ne "") {
+    $ReleaseFullVersion += "-$ReleaseTag"
+}
+
+# Store the release subfolder paths
+$ReleaseMajorPath = "$ReleaseVersionsPath\v$ReleaseMajorVersion"
+$ReleaseMinorPath = "$ReleaseMajorPath\m$ReleaseMinorVersion"
+$ReleaseFullPath = "$ReleaseMinorPath\$ReleaseFullVersion"
+
+# Set the folder for the PyInstaller generated content
+$PyInstallerPath = "$ReleaseFullPath\$PyToolsFolder"
+
+# Before proceeding, confirm the release folder path exists and if not, alert the user to create it
+if (-Not (Test-Path $ReleaseFullPath)) {
+    Write-Host "`nThe release folder $ReleaseFullPath does not exist!`nPlease run the PowerShell script 'CreateReleaseFolder.ps1' before running this script..."
+    Exit
+}
+
+# Before proceeding, confirm if the py-tools path exists and if not, try to create it
+if (-Not (Test-Path $PyInstallerPath)) {
+    # Create the PyInstaller folder
+    Write-Host "`nCreating directory $PyInstallerPath..."
+    Start-Sleep -Seconds 1
+    New-Item $PyInstallerPath -ItemType Directory > $null
+}
+
+Set-Location $PyInstallerPath
+
+# Check to see if there's anything already in the PyInstaller folder and if so, delete it
+if ((Get-ChildItem $PyInstallerPath).Length -gt 0) {
+    Write-Host "`nFound items in the py-tools folder. Deleting all items..."
+    # Loop until there's nothing in py-tools
+    $LoopTrue = $true
+    do {
+        Get-ChildItem -Path $PyInstallerPath -Recurse | ForEach-Object {
+            if ($_.GetType() -eq [System.IO.FileInfo]) {
+                Remove-Item $_
+            }
+            elseif (($_.GetType() -eq [System.IO.DirectoryInfo]) -and ((Get-ChildItem $_).Length -eq 0)) {
+                $LoopTrue = $false
+                continue
+            }
+        }
+    } while ($LoopTrue -eq $true)
+    # Set-Location $PyInstallerPath
+    # Start-Process -FilePath "cmd.exe" -ArgumentList "for /D %v in (*) do rd /s/q %v" -NoNewWindow
+    Write-Host "The folder is now empty."
+}
+
+# Copy over the files and folder necessary to generate the Python executable
+Write-Host "`nCopying over the Python CLI tool and its dependencies to the $PyToolsFolder folder..."
+Start-Sleep $Global:SleepTime
+if ((Get-ChildItem $PyInstallerPath).Length -eq 0) {
+    Copy-Item -Path $DependenciesPath -Destination $PyInstallerPath
+}
+Copy-Item -Path $CSPath -Destination $PyInstallerPath
+Copy-Item -Path $DependenciesPath\*.py -Destination $PyInstallerPath\dependencies
+
+# Let user know the Python executable will be created and count down for 5 seconds
+Write-Host "`nCreating Python executable in..."
+for ($counter = 5; $counter -gt 0; $counter--) {
+    Write-Host "$counter..."
+    Start-Sleep -Seconds 1
+}
+
+# Create the Python executable: pyinstaller .\CompStart.py --onefile
+Start-Process -FilePath $PyIFilePath -ArgumentList $PyIArgumentArray -NoNewWindow -Wait
+Write-Host "`nPython executable successfully created"
+#>
+
+# The following code has been copied from the CopyReleaseContent script:
+<#
+# Initialize the devenv file and folder names to be used in the script
+$DevFolder = "devenv"
+$ConfigFolder = "config"
+$CompStartFolder = "CompStart"
+$PowerShellScriptFile = "CompStart.ps1"
+$BatchScriptFile = "CompStart.bat"
+
+# Create the devenv paths to be used in the script
+$DevPath = "$ProjectRootPath\$DevFolder"
+$ConfigPath = "$DevPath\$ConfigFolder"
+$CSFolderPath = ""
+$CSPowerShellPath = "$DevPath\$PowerShellScriptFile"
+$CSBatchPath = "$DevPath\$BatchScriptFile"
+
+# Initialize the release file and folder names to be used in the script
+$ReleasesFolder = "releases"
+$ReleaseVersionsFolder = "versions"
+$ReleaseTemplatesFolder = "templates"
+$ReleaseNotesFolder = "release-notes"
+$ReleaseNotesMDFile = "release_notes.md"
+$ReleaseInstructionsFile = "instructions.txt"
+$ReleaseFullVersion = ""
+
+# Create the release paths to be used in the script
+$ReleasesPath = "$ProjectRootPath\$ReleasesFolder"
+$ReleaseVersionsPath = "$ReleasesPath\$ReleaseVersionsFolder"
+$ReleaseTemplatesPath = "$ReleasesPath\$ReleaseTemplatesFolder"
+$ReleaseNotesFolderPath = ""
+$ReleaseNotesMDPath = "$ReleaseTemplatesPath\$ReleaseNotesMDFile"
+$ReleaseInstructionsPath = "$ReleaseTemplatesPath\$ReleaseInstructionsFile"
+
+# Create the PyInstaller specific variables to be used in the script
+$PyToolsFolder = "py-tools"
+$PyIDistFolder = "dist"
+$PythonExeFile = "CompStart.exe"
+$PyToolsPath = ""
+$PyIDistPath = ""
+$CSPythonPath = ""
+
+# Determine which version number we are working with
+Write-Host "`nWhat is the release major version number? " -NoNewline
+$ReleaseMajorVersion = $Host.UI.ReadLine()
+
+Write-Host "What is the release minor version number? " -NoNewline
+$ReleaseMinorVersion = $Host.UI.ReadLine()
+
+Write-Host "What is the release tag for v$ReleaseMajorVersion.$ReleaseMinorVersion (or leave blank if there is none)? " -NoNewline
+$ReleaseTag = $Host.UI.ReadLine()
+
+$ReleaseFullVersion = "$ReleaseMajorVersion.$ReleaseMinorVersion"
+
+# Add the release tag if one exists
+if ($ReleaseTag -ne "") {
+    $ReleaseFullVersion += "-$ReleaseTag"
+}
+
+# Store the release subfolder paths
+$ReleaseMajorPath = "$ReleaseVersionsPath\v$ReleaseMajorVersion"
+$ReleaseMinorPath = "$ReleaseMajorPath\m$ReleaseMinorVersion"
+$ReleaseFullPath = "$ReleaseMinorPath\$ReleaseFullVersion"
+
+# Before proceeding, confirm the release folder path exists and if not, alert the user to create it
+if (-Not (Test-Path $ReleaseFullPath)) {
+    Write-Host "`nThe release folder $ReleaseFullPath does not exist!`nPlease run the PowerShell script 'CreateReleaseFolder.ps1' before running this script..."
+    Exit
+}
+
+Set-Location $ReleaseFullPath
+
+# Create the CompStart folder for the release, if needed, and update the appropriate path variable
+if (-Not (Test-Path $CompStartFolder)) {
+    Write-Host "`nCreating the CompStart folder for release $ReleaseFullVersion..."
+    Start-Sleep $Global:SleepTime
+    New-Item -ItemType Directory -Name $CompStartFolder > $null
+}
+else {
+    Write-Host "`nThere already exists a CompStart folder for release $ReleaseFullVersion...skipping this step..."
+    Start-Sleep $Global:SleepTime
+}
+$CSFolderPath = "$ReleaseFullPath\$CompStartFolder"
+
+# Create the release notes folder for the release, if needed, and update the appropriate path variable
+if (-Not (Test-Path $ReleaseNotesFolder)) {
+    Write-Host "`nCreating the release-notes folder for release $ReleaseFullVersion..."
+    Start-Sleep $Global:SleepTime
+    New-Item -ItemType Directory -Name $ReleaseNotesFolder > $null
+}
+else {
+    Write-Host "`nThere already exists a release-notes folder for release $ReleaseFullVersion...skipping this step..."
+    Start-Sleep $Global:SleepTime
+}
+$ReleaseNotesFolderPath = "$ReleaseFullPath\$ReleaseNotesFolder"
+
+# Copy the CompStart content
+Write-Host "`nPopulating the CompStart folder for release $ReleaseFullVersion..."
+Start-Sleep $Global:SleepTime
+Copy-Item -Path $CSBatchPath -Destination $CSFolderPath
+Copy-Item -Path $CSPowerShellPath -Destination $CSFolderPath
+Copy-Item -Path $ConfigPath -Destination $CSFolderPath -Recurse -Force
+
+# Copy the release notes content and instructions file
+Write-Host "`nCopying over the instructions and release notes README for release $ReleaseFullVersion..."
+Start-Sleep $Global:SleepTime
+Copy-Item -Path $ReleaseNotesMDPath -Destination $ReleaseNotesFolderPath
+Copy-Item -Path $ReleaseInstructionsPath -Destination $FullReleasesPath
+
+# Deal with the Python executable
+$PyToolsPath = "$ReleaseFullPath\$PyToolsFolder"
+if (-Not (Test-Path $PyToolsPath)) {
+    Write-Host "`nUnable to find a py-tools folder.`nPlease run the PowerShell script `GeneratePythonTool.ps1` before running this script..."
+    Exit
+}
+$PyIDistPath = "$PyToolsPath\$PyIDistFolder"
+$CSPythonPath = "$PyIDistPath\$PythonExeFile"
+Write-Host "`nCopying over the Python tool executable for release $ReleaseFullVersion..."
+Start-Sleep $Global:SleepTime
+Copy-Item -Path $CSPythonPath -Destination $CSFolderPath
+
+Write-Host "`nAll release content has been copied over successfully to $ReleaseFullPath"
+#>
+
+# The following code has been copied from the GenerateReleasePackage script:
+<#
+# Initialize the static variables to be used in the script
+$PackagesFolder = "packages"
+$PackageVersionsFolder = "versions"
+$ReleasesFolder = "releases"
+$ReleaseVersionsFolder = "versions"
+$CompStartFolder = "CompStart"
+$ReleaseInstructionsFile = "instructions.txt"
+$ReleasesPath = "$ProjectRootPath\$ReleasesFolder"
+$PackagesPath = "$ProjectRootPath\$PackagesFolder"
+$PackageVersionsPath = "$PackagesPath\$PackageVersionsFolder"
+$ReleaseVersionsPath = "$ReleasesPath\$ReleaseVersionsFolder"
+
+
+# Determine which release version to work with
+Write-Host "`nWhat is the release major version number? " -NoNewline
+$ReleaseMajorVersion = $Host.UI.ReadLine()
+
+Write-Host "What is the release minor version number? " -NoNewline
+$ReleaseMinorVersion = $Host.UI.ReadLine()
+
+Write-Host "What is the release tag for v$ReleaseMajorVersion.$ReleaseMinorVersion (or leave blank if there is none)? " -NoNewline
+$ReleaseTag = $Host.UI.ReadLine()
+
+# Create the full release version for later
+$ReleaseFullVersion = "$ReleaseMajorVersion.$ReleaseMinorVersion"
+
+# Add the release tag if one exists
+if ($ReleaseTag -ne "") {
+    $ReleaseFullVersion += "-$ReleaseTag"
+}
+
+# Store the release and package subfolder paths
+$ReleaseMajorPath = "$ReleaseVersionsPath\v$ReleaseMajorVersion"
+$PackageMajorPath = "$PackageVersionsPath\v$ReleaseMajorVersion"
+$ReleaseMinorPath = "$ReleaseMajorPath\m$ReleaseMinorVersion"
+$PackageMinorPath = "$PackageMajorPath\m$ReleaseMinorVersion"
+$ReleaseFullPath = "$ReleaseMinorPath\$ReleaseFullVersion"
+$PackageFullPath = $PackageMinorPath
+
+
+# Before proceeding, confirm the release folder path exists and if not, alert the user to create it
+if (-Not (Test-Path $ReleaseFullPath)) {
+    Write-Host "`nThe release folder $ReleaseFullPath does not exist!`nPlease create the release folder before running this script..."
+    Exit
+}
+
+$ReleaseCSFolderPath = "$ReleaseFullPath\$CompStartFolder"
+$ReleaseInstructionsPath = "$ReleaseFullPath\$ReleaseInstructionsFile"
+
+# Check if the release folder has the necessary folders and files
+if (-Not (Test-Path $ReleaseCSFolderPath) -Or -Not (Test-Path $ReleaseInstructionsPath)) {
+    Write-Host "`nThe release folder $ReleaseFullPath is missing necessary folders and files!`nPlease ensure the release folder has the following folders and files:`n- $CompStartFolder`n- $ReleaseNotesFolder`n- $ReleaseInstructionsFile`n"
+    Exit
+}
+
+# Also check if the packages folder exists
+if (-Not (Test-Path $PackageFullPath)) {
+    Write-Host "`nThe package folder $PackageFullPath does not exist!`nIt will now be created..."
+
+    Set-Location $PackageVersionsPath
+    if (-Not (Test-Path $PackageMajorPath)) {
+        Write-Host "`nCreating the package directory for release major version $ReleaseMajorVersion..."
+        Start-Sleep $Global:SleepTime
+        New-Item -Name "v$ReleaseMajorVersion" -ItemType "directory" > $null
+    }
+
+    Set-Location $PackageMajorPath
+    if (-Not (Test-Path $PackageMinorPath)) {
+        Write-Host "`nCreating the package directory for release minor version $ReleaseMinorVersion..."
+        Start-Sleep $Global:SleepTime
+        New-Item -Name "m$ReleaseMinorVersion" -ItemType "directory" > $null
+    }
+}
+else {
+    Write-Host "`nThe package folder $PackageFullPath already exists`n"
+}
+
+Set-Location $PackageFullPath
+
+# Get the release package name
+$ReleasePackageName = "CompStart-$ReleaseFullVersion.zip"
+
+# Create the hash table object to pass to the Compress-Archive cmdlet
+$PackageContents = @{
+    Path             = $ReleaseCSFolderPath, $ReleaseInstructionsPath
+    DestinationPath  = $ReleasePackageName
+    CompressionLevel = "Optimal"
+}
+
+Write-Host "`nCreating the package artifact for release $ReleaseFullVersion..."
+Start-Sleep $Global:SleepTime
+Compress-Archive @PackageContents > $null
+#>
