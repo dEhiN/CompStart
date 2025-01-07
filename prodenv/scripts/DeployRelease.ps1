e2d80f85a23f98371b1d889b053cf19224eff3ea
Hiran Watson <hiranwatson@gmail.com>
2025-01-07 16:56:12 -0500

    Updated the commented-out, copied code in DeployRelease
    Updated it to reflect the code changes from the previous commit.

--- prodenv/scripts/DeployRelease.ps1
+++ prodenv/scripts/DeployRelease.ps1
@@ -416,18 +416,8 @@ Start-Release
 
 # The following code has been copied from the CreateReleaseFolder script:
 <#
-# Set the starting directory to the project root
-$SetCSSuccess = Set-StartDirectory "CompStart"
-
 # Get the location of the release folder root
 $ProjectRootPath = Get-Location
-
-# Check to make sure we are in the project root
-if (-Not $SetCSSuccess) {
-    # Inform user project root can't be found and the script is ending
-    Write-Host "`nUnable to find project root. Quitting script..."
-    Exit
-}
 
 # Initialize the variables to be used in the script
 $ReleasesFolder = "releases"
