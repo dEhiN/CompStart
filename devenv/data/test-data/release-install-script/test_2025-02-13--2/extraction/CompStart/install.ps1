ecfa69b70849e303233fbef5d0bd2f748ca7dd82
David H. Watson <hiranwatson@gmail.com>
2025-02-13 17:21:44 -0500

    Ran a final test
    Created the folder test_2025-02-13--2 under /devenv/data/test-data/release-install-script. Confirmed that the install.ps1 script works.

---
+++ devenv/data/test-data/release-install-script/test_2025-02-13--2/extraction/CompStart/install.ps1
@@ -0,0 +1,180 @@
+# This will be the installation script used for CompStart
+
+# Script-global variables
+$Script:SleepTime = 2
+$Script:OSSeparatorChar = [System.IO.Path]::DirectorySeparatorChar
+$Script:CSParentPath = [System.Environment]::GetFolderPath('LocalApplicationData')
+$Script:CSFolder = "CompStart"
+$Script:CSFullPath = ""
+$Script:InstallerFolder = "installer-files"
+$Script:FuncRetValue = $false
+
+function New-CSFolder {
+    <#
+        .SYNOPSIS
+            Creates a CompStart folder in the specified path or in the default local application data folder.
+
+        .DESCRIPTION
+            The New-CSFolder function creates a folder named "CompStart" in the path specified by the user. If no path is specified or if the provided path is not valid, the folder will be created in the default local application data folder. If the folder already exists, the function will do nothing.
+
+        .PARAMETER SuppliedPath
+            Optional path where the CompStart folder should be created. If the path is not valid or the parameter is not specified, the folder will be created in the default local application data folder.
+
+        .RETURNS
+            [bool] Returns $true if the folder was created, otherwise $false.
+
+        .EXAMPLE
+            PS> New-CSFolder -SuppliedPath "C:\MyPath"
+            Creates the CompStart folder in "C:\MyPath" if the path is valid.
+
+        .EXAMPLE
+            PS> New-CSFolder
+            Creates the CompStart folder in the default local application data folder.
+
+        .NOTES
+            Author: David H. Watson (with help from VS Code Copilot)
+            GitHub: @dEhiN
+            Date: 2024-12-28
+    #>
+    [CmdletBinding()]
+    param (
+        [string] $SuppliedPath
+    )
+
+    # Test the passed in parameter and use it if it's a valid path
+    if ($SuppliedPath) {
+        if (-Not (Test-Path -Path $SuppliedPath)) {
+            Write-Host "$SuppliedPath is not a valid file system path...defaulting to $Script:CSParentPath..."
+        }
+        else {
+            $Script:CSParentPath = $SuppliedPath
+        }
+    }
+
+    # Set the full CompStart folder path
+    $Script:CSFullPath = $Script:CSParentPath + $Script:OSSeparatorChar + $Script:CSFolder
+
+    # Create the folder if need be
+    if (-Not (Test-Path $CSFullPath)) {
+        Write-Host "`nCreating CompStart folder..." -NoNewline
+        Start-Sleep $Script:SleepTime
+        New-Item -Path $CSFullPath -ItemType "Directory" > $null
+        Write-Host "...folder successfully created at $CSFullPath"
+        $Script:FuncRetValue = $true
+    }
+    else {
+        Write-Host "`nExisting CompStart folder found at $CSFullPath..." -NoNewline
+        Start-Sleep $Script:SleepTime
+        Write-Host "...skipping this step"
+    }
+
+    return $Script:FuncRetValue
+}
+
+function Install-CSFiles {
+    <#
+        .SYNOPSIS
+            Installs the files required for CompStart.
+
+        .DESCRIPTION
+            The Install-CSFiles function installs the files required for CompStart. The files are copied from the installer-files folder to the CompStart folder. If the files already exist in the CompStart folder, they will be overwritten.
+
+        .RETURNS
+            [bool] Returns $true if the files were successfully installed, otherwise $false.
+
+        .EXAMPLE
+            PS> Install-CSFiles
+            Installs the files required for CompStart.
+
+        .NOTES
+            Author: David H. Watson (with help from VS Code Copilot)
+            GitHub: @dEhiN
+            Date: 2024-12-28
+    #>
+
+    Write-Host "`nStarting installation of CompStart..."
+    Start-Sleep $Script:SleepTime
+
+    # Get a list of all the files to install
+    $InstallerFullPath = $PSScriptRoot + $Script:OSSeparatorChar + $Script:InstallerFolder
+    $InstallerFilesList = Get-ChildItem -Recurse $InstallerFullPath
+
+    # Set the initial destination path
+    $DestPath = $Script:CSFullPath
+ 
+    # Create the directory structure for the CompStart folder
+    Write-Host "`nCreating the folder directory structure..."
+    Start-Sleep $Script:SleepTime
+    foreach ($Item in $InstallerFilesList) {
+        if ($Item.PSIsContainer) {
+            $DestPath = $DestPath + $Script:OSSeparatorChar + $Item.Name
+            if (-Not (Test-Path $DestPath)) {
+                Write-Host "Creating $($Item.Name) folder..." -NoNewline
+                Start-Sleep $Script:SleepTime
+                New-Item -Path $DestPath -ItemType "Directory" > $null
+                Write-Host "...folder successfully created at $DestPath"
+            }
+            else {
+                Write-Host "Existing $($Item.Name) folder found at $DestPath..." -NoNewline
+                Start-Sleep $Script:SleepTime
+                Write-Host "...skipping this step"
+            }
+        }
+    }
+
+    # Reset the destination path
+    $DestPath = $Script:CSFullPath
+
+    # Copy over all the files to the CompStart folder
+    Write-Host "`nCopying over the CompStart files..." -NoNewline
+    Start-Sleep $Script:SleepTime    
+    foreach ($Item in $InstallerFilesList) {
+        if (-not $Item.PSIsContainer) {
+            # Get the parent folder of the current item
+            $ItemPathArray = $Item.PSParentPath.Split("\")
+            $ItemParentFolder = $ItemPathArray[$ItemPathArray.Length - 1]
+
+            # Get the current (working) folder in the destination path
+            $DestPathArray = $DestPath.Split("\")
+            $DestCurrentFolder = $DestPathArray[$DestPathArray.Length - 1]
+
+            # Determine which situation is present to make sure the file is copied to the correct location
+            if ($ItemParentFolder -eq $Script:InstallerFolder) {
+                Write-Host "`nInstalling $($Item.Name) to $DestPath..." -NoNewline
+                Start-Sleep $Script:SleepTime
+                Copy-Item -Path $Item.FullName -Destination $DestPath -Force
+                Write-Host "...successfully installed $($Item.Name) to $DestPath"
+                $DestPath = $DestPath + $Script:OSSeparatorChar + $DestCurrentFolder
+            } 
+            elseif ($ItemParentFolder -eq $DestCurrentFolder) {
+                Write-Host "`nInstalling $($Item.Name) to $DestPath..." -NoNewline
+                Start-Sleep $Script:SleepTime
+                Copy-Item -Path $Item.FullName -Destination $DestPath -Force
+                Write-Host "...successfully installed $($Item.Name) to $DestPath"
+            }
+            else {
+                $DestPath = $DestPath + $Script:OSSeparatorChar + $ItemParentFolder
+                Write-Host "`nInstalling $($Item.Name) to $DestPath..." -NoNewline
+                Start-Sleep $Script:SleepTime
+                Copy-Item -Path $Item.FullName -Destination $DestPath -Force
+                Write-Host "...successfully installed $($Item.Name) to $DestPath"
+            }
+        }
+    }
+}
+
+# Main script logic
+
+# Check if the current environment is production or not
+$IsProdEnv = $false
+
+if ($IsProdEnv) {
+    Write-Host "Production isn't ready yet. Try again later..."
+    Exit
+}
+
+# Create the CompStart folder if required
+New-CSFolder > $null
+
+# Install the required files
+Install-CSFiles  > $null
---
+++ devenv/data/test-data/release-install-script/test_2025-02-13--2/releases/v2/m1/2.1/py-tool/CompStart.py
@@ -0,0 +1,118 @@
+# This will be a command line tool to create and edit the startup_data.json file
+import sys
+import dependencies.cs_jsonfn as deps_json
+import dependencies.cs_helper as deps_helper
+import dependencies.cs_chooser as deps_chooser
+import dependencies.cs_pretty as deps_pretty
+
+# Global Variables
+
+# Specifies whether the tool is in production or testing
+is_prod = True
+
+# Specifies the name of the project root or the start directory
+start_dir = "CompStart"
+
+# If there are any errors, print this out at the end
+final_err_msg = (
+    "Please see the error message(s) above and report them to the development team"
+)
+
+# Program starting point
+if __name__ == "__main__":
+    # Set the starting directory
+    start_dir_result = deps_helper.set_start_dir(start_dir)
+    if not start_dir_result:
+        deps_pretty.prettify_custom_error(
+            "No folder called CompStart could be found on the current path\nExiting tool...",
+            "main > set_start_dir",
+        )
+        print(f"\n{final_err_msg}")
+        input("\nPlease press the enter key when ready to close this window...")
+        sys.exit()
+
+    # Variables for location and name of JSON file with startup data
+    json_path = deps_helper.get_prod_path()
+    json_filename = deps_helper.get_startup_filename(default_json=False)
+
+    # Print welcome message
+    print(
+        "\nWelcome to CompStart: The computer startup tool that will make your life easier"
+    )
+
+    # Initialize status variables
+    status_state = False
+    status_message = "No action taken..."
+    quit_loop = False
+
+    # Menu choices to present to user during main loop
+    menu_choices = [
+        "Program description",
+        "Create a new startup file",
+        "View the startup file",
+        "Edit the startup file",
+        # "Add new startup items to the startup file",
+    ]
+
+    # Main loop to allow user to navigate program options
+    while not quit_loop:
+        user_choice = deps_chooser.user_menu_chooser(menu_choices)
+
+        match user_choice:
+            case 1:
+                deps_helper.program_info()
+            case 2:
+                # Find out which type of new file the user wants
+                is_default, create_file = deps_chooser.new_file_chooser()
+
+                if create_file:
+                    # Create a new JSON file
+                    status_state, status_message = deps_json.json_creator(
+                        json_path, json_filename, is_default
+                    )
+
+                # If there were any errors, let the user know to check the error messages
+                if not status_state and not status_message.startswith("Skipped"):
+                    status_message = final_err_msg
+
+                # Print out the status message
+                print(f"\n{status_message}")
+            case 3:
+                # Read in existing JSON file and store the return results of the json_read function, then print out if the read was successful
+                status_state, status_message, json_data = deps_json.json_reader(
+                    json_path, json_filename
+                )
+
+                # If there were any errors, let the user know to check the error messages
+                if not status_state:
+                    status_message = final_err_msg
+
+                # Print out the status message
+                print(f"\n{status_message}")
+
+                # If there was data read in, print it out in a prettified way
+                if status_state:
+                    input("Press enter when ready to view the startup data...")
+                    print(deps_pretty.prettify_json(json_data))
+                    input(
+                        "\nPress enter when ready to return to the previous menu..."
+                    )
+            case 4:
+                status_state, status_message = deps_json.json_editor(
+                    json_path, json_filename
+                )
+
+                # If there were any errors, let the user know to check the error messages
+                if not status_state:
+                    status_message = final_err_msg
+
+                # Print out the status message
+                print(f"\n{status_message}")
+
+            # case 5:
+            #    deps_json.json_adder(json_path, json_filename)
+            case _:
+                # This case will never really be addressed since the function user_menu_chooser adds an option by default to quit the program
+                # If the user picks that option, the function calls sys.exit so execution should never return to this loop
+                # However, just in case execution does return (i.e., some bug that gets introduced), this will prevent an infinite loop
+                quit_loop = True
