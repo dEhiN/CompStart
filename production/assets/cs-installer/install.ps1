59374c3401e7cf5866a4600c3af20cf929490585
Hiran Watson <hiranwatson@gmail.com>
2026-06-25 23:44:24 -0400

    Removed the file old-install.ps1 from /production/assets/cs-installer and copied over the changes for the installation script from the install.ps1 in the 2.1-test release to the one in the cs-installer folder

--- production/assets/cs-installer/install.ps1
+++ production/assets/cs-installer/install.ps1
@@ -4,8 +4,11 @@
 $Script:SleepTime = 2
 $Script:OSSeparatorChar = [System.IO.Path]::DirectorySeparatorChar
 $Script:CSParentPath = [System.Environment]::GetFolderPath('LocalApplicationData')
+$Script:WinStartupFolderPath = [System.Environment]::GetFolderPath('Startup')
 $Script:CSFolder = "CompStart"
 $Script:CSFullPath = ""
+$Script:CSShortcutName = "CompStart.lnk"
+$Script:CSShortcutTarget = "CompStart.bat"
 $Script:InstallerFolder = "installer-files"
 
 function New-CSFolder {
@@ -139,6 +142,98 @@ function Install-CSFiles {
     return $FuncRetValue
 }
 
+function New-CSShortcut {
+    <#
+        .SYNOPSIS
+            Creates a new shortcut or symlink for CompStart.ps1 in the OS's startup programs area.
+
+        .DESCRIPTION
+            The New-CSShortcut function creates a shortcut for CompStart.p1 and places it in the operating system's area for programs that start when the user logs in. The function takes an optional parameter that specifies which operating system is being worked with. If the parameter isn't provided, the function defaults to Windows. Helper functions are used to create the OS specific startup shortcuts.
+
+        .PARAMETER OperatingSystem
+            Optional string parameter specifying the operating system to work with. If the parameter is not present, the default operating system of Windows is used. Note: Currently, only Windows works as the OS.
+
+        .RETURNS
+            [bool] Returns $true if the shortcut was created, otherwise $false.
+
+        .EXAMPLE
+            PS> New-CSShortcut
+            Assumes the operating system is Windows and creates a link to CompStart.ps1 in C:\Users\<username>\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+
+        .EXAMPLE
+            PS> New-CSShortcut -OperatingSystem "Windows"
+            Creates a link to CompStart.ps1 in C:\Users\<username>\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+
+        .NOTES
+            Author: David H. Watson
+            GitHub: @dEhiN
+            Date: 2026-06-20
+    #>
+    [CmdletBinding()]
+    param (
+        [string] $OperatingSystem
+    )
+
+    # Set up the return flag variable
+    $FuncRetValue = $false
+
+    # Specify which type of OS to work with:
+    # Windows = 1
+    $OSType = 1
+
+    if ($OSType -eq 1) {
+        Write-Host "`nCreating the Windows startup shortcut link..."
+        Start-Sleep $Script:SleepTime
+        $FuncRetValue = New-WindowsStartShortcut
+    }
+
+    return $FuncRetValue
+}
+
+function New-WindowsStartShortcut {
+    <#
+        .SYNOPSIS
+            Creates a new Windows shortcut for CompStart.ps1 in the user's startup folder.
+
+        .DESCRIPTION
+            The New-WindowsStartShortcut function creates a Windows shortcut for CompStart.ps1 and stores it in the user's startup folder. Specifically, it is stored in the location that the Windows Run command "shell:startup" returns, which is usually the Startup folder for the user Start Menu.
+
+        .RETURNS
+            [bool] Returns $true if the shortcut was created, otherwise $false.
+
+        .EXAMPLE
+            PS> New-WindowsStartShortcut
+            Creates a link to CompStart.ps1 in C:\Users\<username>\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+
+        .NOTES
+            Author: David H. Watson
+            GitHub: @dEhiN
+            Date: 2026-06-24
+    #>
+
+    # Confirm the Windows Startup folder location exists
+    if (-Not $Script:WinStartupFolderPath) {
+        # A startup folder doesn't exist, so not creating the shortcut
+        Write-Host "`nCannot determine the location of the Start folder..."
+        return $false
+    }
+
+    # Create the COM Object to use for generating the shortcut
+    $WshShell = New-Object -ComObject WScript.Shell
+
+    # Use the built-in method to create a shortcut
+    $CSWindowsShortcut = $WshShell.CreateShortcut($Script:WinStartupFolderPath + $Script:OSSeparatorChar + $Script:CSShortcutName)
+
+    # Set the target path to CompStart.bat
+    $CSWindowsShortcut.TargetPath = $Script:CSFullPath + $Script:OSSeparatorChar + $Script:CSShortcutTarget
+
+    # Save or create the shortcut
+    $CSWindowsShortcut.Save()
+
+    return $true
+}
+
+
 # Main script logic
 
 # Check if the current environment is production or not
@@ -155,8 +250,11 @@ $CSFolderSuccess = New-CSFolder
 # Install the required files
 $CSFilesSuccess = Install-CSFiles
 
+# Create the CompStart.ps1 shortcut link
+$CSShortcutSuccess = New-CSShortcut
+
 # Let the user know the results of the installation
-if ($CSFolderSuccess -and $CSFilesSuccess) {
+if ($CSFolderSuccess -and $CSFilesSuccess -and $CSShortcutSuccess) {
     Write-Host "`n...CompStart has been fully installed!"
 }
 else {
