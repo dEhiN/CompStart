b571392ed737abac4f4a9fd40300797ff83c29a5
Hiran Watson <hiranwatson@gmail.com>
2026-09-15 22:23:06 -0400

    Decided to make the 3 variables into an array for future scalability

--- production/synchronise-branches.sh
+++ production/synchronise-branches.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # Define the 3 master branch names
-main_branch="main"
-release_branch="releases"
-qa_branch="qa-testing-debug"
+master_branches=("main" "releases" "qa-testing-debug")
 
 # Get the command line argument specifying which branch is most up to date
 sync_branch=$1
