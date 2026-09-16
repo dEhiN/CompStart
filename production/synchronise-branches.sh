3afb8763d562c413e759ee446c1626f504340a14
Hiran Watson <hiranwatson@gmail.com>
2026-09-15 21:58:22 -0400

    Added a little validation to the argument prior to proceeding

--- production/synchronise-branches.sh
+++ production/synchronise-branches.sh
@@ -7,3 +7,14 @@ qa_branch="qa-testing-debug"
 
 # Get the command line argument specifying which branch is most up to date
 sync_branch=$1
+
+# Do some validation on the argument
+if [ -z $sync_branch ]; then
+    echo "Missing branch name argument..."
+    exit
+fi
+
+if [ $sync_branch != $main_branch ] && [ $sync_branch != $release_branch ] && [ $sync_branch != $qa_branch ]; then
+    echo "Invalid branch name argument..."
+    exit
+fi
