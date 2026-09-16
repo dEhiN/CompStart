1aedd0bace0955d3234ddf70aff320cb912c0ea0
Hiran Watson <hiranwatson@gmail.com>
2026-09-15 21:00:31 -0400

    Minor fix

--- production/DeployRelease.ps1
+++ production/DeployRelease.ps1
@@ -477,7 +477,7 @@ function Copy-ReleaseContents {
     Copy-Item -Path $Script:AllPaths.AssetInstructionsText  -Destination $Script:AllPaths.ReleaseCSFolder
 
     # Rename the release notes file
-    $ReleaseNotesOldName = "$(Script:FileNames.ReleaseNotesMarkdown)"
+    $ReleaseNotesOldName = "$($Script:FileNames.ReleaseNotesMarkdown)"
     $ReleaseNotesFilePath = "$($Script:AllPaths.ReleaseNotesFolder)$($Script:OSSeparatorChar)$($ReleaseNotesOldName)"
     $ReleaseNotesNewName = "$($ReleaseNotesOldName.Substring(0, $ReleaseNotesOldName.Length - 3))_$($ReleaseFullVersion).md"
     Rename-Item -Path $ReleaseNotesFilePath -NewName $ReleaseNotesNewName
