cf1c4552337ffc9b9215cf23a820ce1448d8349d
Hiran Watson <hiranwatson@gmail.com>
2026-09-15 21:49:06 -0400

    Started coding in the script

--- production/synchronise-branches.sh
+++ production/synchronise-branches.sh
@@ -1,1 +1,9 @@
 #!/bin/sh
+
+# Define the 3 master branch names
+main_branch="main"
+releases_branch="releases"
+qa_branch="qa-testing-debug"
+
+# Get the command line argument specifying which branch is most up to date
+sync_branch=$1
